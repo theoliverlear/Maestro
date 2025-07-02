f2c6d123efa2a3f2d12b96b26eb7c9a99ecbbabe
theoliverlear <sigwartholiver2@gmail.com>
2025-07-02 17:18:08 -0500

    Agregue Conjugator.cs para crear un diccionario con métodos de servicio para la conjugación de verbos

---
+++ Maestro/Api/Modelos/Conjugación/Conjugador.cs
@@ -0,0 +1,64 @@
+using System.Globalization;
+using CsvHelper;
+using CsvHelper.Configuration;
+
+namespace Maestro.Api.Modelos.Conjugación;
+
+using DiccionarioDeConjugación = Dictionary<string, Dictionary<string,
+                                 Dictionary<string, FilaDeConjugación>>>;
+
+public class Conjugador
+{
+    public static DiccionarioDeConjugación Diccionario = InicializarDiccionario();
+
+    private static DiccionarioDeConjugación InicializarDiccionario()
+    {
+        using CsvReader csv = InicializarCsv();
+        IEnumerable<FilaDeConjugación> filas = csv.GetRecords<FilaDeConjugación>();
+        DiccionarioDeConjugación diccionario = filas
+            .GroupBy(fila => fila.Infinitivo)
+            .ToDictionary(groupo => groupo.Key,
+                groupo => groupo.GroupBy(fila => fila.Ánimo)
+                    .ToDictionary(
+                        groupoDeEstadoÁnimo => groupoDeEstadoÁnimo.Key,
+                        groupoDeEstadoÁnimo =>
+                            groupoDeEstadoÁnimo.ToDictionary(tenso => tenso.Tenso,
+                                tenso => tenso)));
+        return diccionario;
+    }
+
+    private static CsvReader InicializarCsv()
+    {
+        CsvReader? csv = null;
+        try
+        {
+            string camino = Path.Combine(AppContext.BaseDirectory, @"Api\Modelos\Conjugación\conjugaciones.csv");
+            using StreamReader lectora = new StreamReader(camino);
+            csv = new CsvReader(lectora, new CsvConfiguration(CultureInfo.InvariantCulture)
+            {
+                Delimiter = ",",
+                BadDataFound = null,
+                MissingFieldFound = null,
+                TrimOptions = CsvHelper.Configuration.TrimOptions.Trim
+            });
+            return csv;
+        }
+        catch
+        {
+            csv?.Dispose();
+            throw;
+        }
+    }
+
+    public static string ObtenerPorPronombre(string verbo, Pronombre pronombre)
+    {
+        return string.Empty;
+    }
+
+    public static string ObtenerYo(string verbo)
+    {
+        return Diccionario[verbo]["Indicativo"]["Presente"].Yo;
+    }
+
+    private string verbo;
+}
