90b5bdccf16309bc4ccebe59a61d25be75569cf0
theoliverlear <sigwartholiver2@gmail.com>
2025-07-02 09:13:22 -0500

    Agregue método `ObtenerPalabras()` en ControladorDeCasa.cs para retornar una lista generada por la clase `MensajeDeAi` y ajuste la ruta base del controlador a `api/casa`

--- Maestro/Api/Controlador/ControladorDeCasa.cs
+++ Maestro/Api/Controlador/ControladorDeCasa.cs
@@ -1,22 +1,31 @@
+using Maestro.Api.Modelos.Ai;
 using Microsoft.AspNetCore.Mvc;
 
 namespace Maestro.Api.Controlador;
 
 [ApiController]
-[Route("api/[controller]")]
+[Route("api/casa")]
 public class ControladorDeCasa : ControllerBase
 {
-    private static readonly List<string> alumnos =
+    private static readonly List<string> Alumnos =
     [
         "Oliver Sigwarth"
     ];
 
     [HttpGet]
-    public ActionResult<IEnumerable<string>> ObtenerTodo() => Ok(alumnos);
+    public ActionResult<IEnumerable<string>> ObtenerTodo() => Ok(Alumnos);
 
     [HttpGet("alumnos")]
     public ActionResult<List<string>> ObtenerAlumnos()
     {
-        return Ok(alumnos);
+        return Ok(Alumnos);
+    }
+
+    [HttpGet("ai/palabras")]
+    public ActionResult<string> ObtenerPalabras()
+    {
+        MensajeDeAi mensaje = new MensajeDeAi();
+        string respuesta = mensaje.ObtenerListaDePalabras().Result;
+        return Ok(respuesta);
     }
 }
