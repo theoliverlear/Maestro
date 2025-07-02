4d13bb763849238b6bdd9b0f87db4bb8e7052dfa
theoliverlear <sigwartholiver2@gmail.com>
2025-07-02 11:48:50 -0500

    Agregue App.tsx para definir el componente raíz `App` en React

---
+++ react/src/componentes/app/App.tsx
@@ -0,0 +1,12 @@
+import './App.scss';
+import type {ReactElement} from "react";
+
+function App(): ReactElement {
+  return (
+      <>
+        <h1>Maestro</h1>
+      </>
+  );
+}
+
+export default App
