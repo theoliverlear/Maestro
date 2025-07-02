2db0af5c5ad5cf41c8df41b8e97a88937af98468
theoliverlear <sigwartholiver2@gmail.com>
2025-07-02 17:24:15 -0500

    Agregue PruebasDeSufijoVerbal.cs para probar la funcionalidad de la clase `SufijoVerbal`

---
+++ Maestro/Prueba/Modelos/Verbos/PruebasDeSufijoVerbal.cs
@@ -0,0 +1,32 @@
+using Maestro.Api.Modelos.Verbos;
+
+namespace Maestro.Prueba.Modelos.Verbos;
+
+using Xunit;
+
+public class PruebasDeSufijoVerbal
+{
+    [Fact]
+    public void SufijoVerbal_Constantes_NoEsNulo()
+    {
+        Assert.NotNull(SufijoVerbal.Ar);
+        Assert.NotNull(SufijoVerbal.Er);
+        Assert.NotNull(SufijoVerbal.Ir);
+    }
+
+    [Fact]
+    public void SufijoVerbal_Constantes_SufijosCorrectos()
+    {
+        Assert.Equal("ar", SufijoVerbal.Ar.Sufijo);
+        Assert.Equal("er", SufijoVerbal.Er.Sufijo);
+        Assert.Equal("ir", SufijoVerbal.Ir.Sufijo);
+    }
+
+    [Fact]
+    public void ObtenerSufijo_Er_SufijoCorrecto()
+    {
+        const string verbo = "comer";
+        SufijoVerbal sufijo = SufijoVerbal.ObtenerSufijo(verbo);
+        Assert.Equal(SufijoVerbal.Er, sufijo);
+    }
+}
