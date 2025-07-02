8e52f72e1dbbd25bcd1fe4f72e7fe1ac19d9d204
theoliverlear <sigwartholiver2@gmail.com>
2025-07-02 09:11:56 -0500

    Agregue ContraseñaSegura.cs para codificar y almacenar contraseñas de forma segura en la API .NET

---
+++ Maestro/Api/Entidad/Usuario/ContraseñaSegura.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Identity;
+
+using Microsoft.AspNetCore.Identity;
+namespace Maestro.Api.Entidad.Usuario;
+
+public class ContraseñaSegura
+{
+    private long id;
+    private string contraseñaCodificada;
+
+    public ContraseñaSegura(string contraseñaSinCodificar)
+    {
+        this.contraseñaCodificada = this.Codificar(contraseñaSinCodificar);
+    }
+
+    public string Codificar(string contraseñaSinCodificar)
+    {
+        PasswordHasher<ContraseñaSegura> picadora = new PasswordHasher<ContraseñaSegura>();
+        return picadora.HashPassword(this, contraseñaSinCodificar);
+    }
+
+    public string ContraseñaCodificada
+    {
+        get; set;
+    }
+}
