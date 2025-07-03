d9e6a8182c78e8b043f6308658a7959c5f346326
theoliverlear <sigwartholiver2@gmail.com>
2025-07-03 09:13:20 -0500

    Agregue BarraDeNav.tsx como componente de barra de navegación en React

---
+++ React/src/componentes/elementos/grupos-elementos-nav/barra-de-nav/BarraDeNav.tsx
@@ -0,0 +1,12 @@
+import type {ReactElement} from "react";
+import "./BarraDeNav.scss";
+
+function BarraDeNav(): ReactElement {
+    return (
+        <div className={"barra-de-nav"}>
+
+        </div>
+    )
+}
+
+export default BarraDeNav;
