7a420b6a06c385c2a43c8168ac02bce0e0065030
theoliverlear <sigwartholiver2@gmail.com>
2025-07-03 09:10:40 -0500

    Expanda App.tsx para integrar rutas, barra de navegación y componentes de página

--- React/src/componentes/app/App.tsx
+++ React/src/componentes/app/App.tsx
@@ -1,12 +1,23 @@
 import './App.scss';
 import type {ReactElement} from "react";
+import {Route, BrowserRouter as Router, Routes} from "react-router-dom";
+import Casa from "../páginas/casa/Casa.tsx";
+import Conjugación from "../páginas/conjugación/Conjugación.tsx";
+import BarraDeNav
+    from "../elementos/grupos-elementos-nav/barra-de-nav/BarraDeNav.tsx";
 
 function App(): ReactElement {
-  return (
-      <>
-        <h1>Maestro</h1>
-      </>
-  );
+    return (
+        <div className={"app"}>
+            <BarraDeNav/>
+            <Router>
+                <Routes>
+                    <Route path={"/"} element={<Casa/>}/>
+                    <Route path={"/conj"} element={<Conjugación/>}/>
+                </Routes>
+            </Router>
+        </div>
+    );
 }
 
 export default App
