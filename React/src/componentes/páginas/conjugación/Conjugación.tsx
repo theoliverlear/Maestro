7db0b470ecf6e8c538b5c714b7351d24f7752deb
theoliverlear <sigwartholiver2@gmail.com>
2025-07-03 09:16:13 -0500

    Agregue Conjugación.tsx como la página de herramientas de conjugación en React

---
+++ React/src/componentes/páginas/conjugación/Conjugación.tsx
@@ -0,0 +1,12 @@
+import "./Conjugación.scss";
+import type {ReactElement} from "react";
+
+function Conjugación(): ReactElement {
+    return (
+        <div className={"conjugación"}>
+
+        </div>
+    );
+}
+
+export default Conjugación;
