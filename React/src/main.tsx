298dde49d7ac1ed3feb74575a0ea372a7d1e625a
theoliverlear <sigwartholiver2@gmail.com>
2025-07-03 15:00:38 -0500

    Agregue `ProveedorDeInyecciones` en main.tsx para habilitar inyección de dependencias usando `tsyringe`

--- React/src/main.tsx
+++ React/src/main.tsx
@@ -1,9 +1,14 @@
+import "reflect-metadata";
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import App from './componentes/app/App.tsx';
+import {ProveedorDeInyecciones} from "./servicios/id/ProveedorDeServicios.ts";
+import {container} from "tsyringe";
 
 createRoot(document.getElementById('root')!).render(
     <StrictMode>
-        <App/>
+        <ProveedorDeInyecciones value={container}>
+            <App/>
+        </ProveedorDeInyecciones>
     </StrictMode>,
 );
