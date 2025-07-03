1ae61a35385eb839da4b1d0f9e466e586b414e28
theoliverlear <sigwartholiver2@gmail.com>
2025-07-03 18:07:48 -0500

    Edite Casa.tsx para incluir los componentes `Título` y `Subtítulo` para la introducción del título de la aplicación

--- React/src/componentes/páginas/casa/Casa.tsx
+++ React/src/componentes/páginas/casa/Casa.tsx
@@ -1,10 +1,19 @@
 import './Casa.scss';
 import type {ReactElement} from "react";
+import Título from "../../elementos/grupos-elementos-texto/título/Título.tsx";
+import {TipoDeEtiqueta} from "../../../modelos/html/TipoDeEtiqueta.ts";
+import Subtítulo
+    from "../../elementos/grupos-elementos-texto/subtítulo/Subtítulo.tsx";
 
 function Casa(): ReactElement {
     return (
         <div className={"casa"}>
-
+            <div className={"texto-de-apertura-casa"}>
+                <Título texto={"¡Maestro!"}
+                        tipoDeEtiqueta={TipoDeEtiqueta.H1}/>
+                <Subtítulo texto={"Un punto de inflexión en la educación española."}
+                           tipoDeEtiqueta={TipoDeEtiqueta.H5}/>
+            </div>
         </div>
     );
 }
