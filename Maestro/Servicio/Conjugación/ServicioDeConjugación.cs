f801aa0dd6a894acf83e66ed2803ed186b78180c
theoliverlear <sigwartholiver2@gmail.com>
2025-07-03 09:31:28 -0500

    Agregue ServicioDeConjugación.cs para implementar métodos de conjugación de verbos en un servicio con inyección de dependencia

---
+++ Maestro/Servicio/Conjugación/ServicioDeConjugación.cs
@@ -0,0 +1,18 @@
+using Maestro.Modelos;
+using Maestro.Modelos.Conjugación;
+using Maestro.Servicio.Conjugación;
+
+namespace Maestro.Servicio.Conjugación;
+
+public class ServicioDeConjugación : IServicioDeConjugación
+{
+    public string Conjugar(string verbo, string pronombre)
+    {
+        return this.Conjugar(verbo, Pronombre.DeCuerda(pronombre));
+    }
+
+    public string Conjugar(string verbo, Pronombre pronombre)
+    {
+        return Conjugador.ObtenerPorPronombre(verbo, pronombre);
+    }
+}
