f85819b92a9fbd512ee738242a28a7a543f3919b
theoliverlear <sigwartholiver2@gmail.com>
2025-07-03 09:19:06 -0500

    Agregue ElementoDeNav.tsx como un componente de pestaña de navegación en React

---
+++ React/src/componentes/elementos/grupos-elementos-nav/elemento-de-nav/ElementoDeNav.tsx
@@ -0,0 +1,10 @@
+import type {ReactElement} from "react";
+
+function ElementoDeNav(): ReactElement {
+    return (
+        <div className={"elemento-de-nav"}>
+
+        </div>
+    );
+}
+export default ElementoDeNav;
