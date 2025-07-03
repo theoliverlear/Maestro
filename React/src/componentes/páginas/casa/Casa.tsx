0ecddcaa8024029558c4fbf49bf545eba19be40a
theoliverlear <sigwartholiver2@gmail.com>
2025-07-03 09:14:16 -0500

    Agregue Casa.tsx como un componente de página de inicio en React

---
+++ React/src/componentes/páginas/casa/Casa.tsx
@@ -0,0 +1,12 @@
+import './Casa.scss';
+import type {ReactElement} from "react";
+
+function Casa(): ReactElement {
+    return (
+        <div className={"casa"}>
+
+        </div>
+    );
+}
+
+export default Casa;
