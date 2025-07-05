f0b972454fe08409e9e7966050a467829963c142
theoliverlear <sigwartholiver2@gmail.com>
2025-07-05 18:27:11 -0500

    Agregue TipoDeEntrada.ts para definir un enumerado de tipos de entrada en formularios de HTML

---
+++ React/src/componentes/elementos/grupos-elementos-nativos/entrada/models/TipoDeEntrada.ts
@@ -0,0 +1,10 @@
+export enum TipoDeEntrada {
+    NÚMERO = 'number',
+    TEXTO = 'text',
+    CONTRASEÑA = 'password',
+    CORREO_ELECTRÓNICO = 'email',
+    TECLADO_TELEFÓNICO = 'tel',
+    CAJA = 'checkbox',
+    RANGO = 'range',
+    FECHA = 'date',
+}
