b02cec6e66988d31396859b55bd8c849ec48fd46
theoliverlear <sigwartholiver2@gmail.com>
2025-07-05 10:21:32 -0500

    Edite ElementoDeNav.tsx para agregar props `texto` y `enlace`, envoltura con `Link`, y usar `Título` para mejorar la estructura y la funcionalidad

--- React/src/componentes/elementos/grupos-elementos-nav/elemento-de-nav/ElementoDeNav.tsx
+++ React/src/componentes/elementos/grupos-elementos-nav/elemento-de-nav/ElementoDeNav.tsx
@@ -1,10 +1,21 @@
 import type {ReactElement} from "react";
+import Título from "../../grupos-elementos-texto/título/Título.tsx";
+import {TipoDeEtiqueta} from "../../../../modelos/html/TipoDeEtiqueta.ts";
+import "./ElementoDeNav.scss";
+import {Link} from "react-router-dom";
 
-function ElementoDeNav(): ReactElement {
+interface PropsElementoDeNav {
+    texto: string;
+    enlace: string;
+}
+
+function ElementoDeNav(props: PropsElementoDeNav): ReactElement {
     return (
-        <div className={"elemento-de-nav"}>
-
-        </div>
+        <Link to={props.enlace} className={"enlace-sin-estilo"}>
+            <div className={"elemento-de-nav"}>
+                <Título texto={props.texto} tipoDeEtiqueta={TipoDeEtiqueta.H4}/>
+            </div>
+        </Link>
     );
 }
 export default ElementoDeNav;
