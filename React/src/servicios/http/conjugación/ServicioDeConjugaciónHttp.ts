30e6622371deec861165711e1ab384c5162529b9
theoliverlear <sigwartholiver2@gmail.com>
2025-07-05 18:26:13 -0500

    Edite ServicioDeConjugaciónHttp.ts para cambiar `VerboConjugado` a `VerboConjugadoHttp` y actualizar la URL base

--- React/src/servicios/http/conjugación/ServicioDeConjugaciónHttp.ts
+++ React/src/servicios/http/conjugación/ServicioDeConjugaciónHttp.ts
@@ -3,17 +3,17 @@ import {ClienteHttp} from "../ClienteHttp.ts";
 import type {
     Pronombre,
     Verbo,
-    VerboConjugado
+    VerboConjugado, VerboConjugadoHttp
 } from "../../../modelos/conjugación/tipos.ts";
 
 @injectable()
-export class ServicioDeConjugaciónHttp extends ClienteHttp<any, VerboConjugado> {
-    private static readonly URL: string = "/casa/conj/";
+export class ServicioDeConjugaciónHttp extends ClienteHttp<any, VerboConjugadoHttp> {
+    private static readonly URL: string = "/conj/";
     constructor() {
         super(ServicioDeConjugaciónHttp.URL);
     }
 
-    public async obtenerConjugación(verbo: Verbo, pronombre: Pronombre): Promise<VerboConjugado> {
+    public async obtenerConjugación(verbo: Verbo, pronombre: Pronombre): Promise<VerboConjugadoHttp> {
         this.url = `${ServicioDeConjugaciónHttp.URL}${verbo}/${pronombre}`;
         return await this.get();
     }
