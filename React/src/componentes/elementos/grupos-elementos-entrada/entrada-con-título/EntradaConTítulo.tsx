13aa79ed7f083052267409c5642e1b64443839a2
theoliverlear <sigwartholiver2@gmail.com>
2025-07-05 18:20:09 -0500

    Agregue EntradaConTítulo.tsx como componente React para combinar un título y un campo de entrada

---
+++ React/src/componentes/elementos/grupos-elementos-entrada/entrada-con-título/EntradaConTítulo.tsx
@@ -0,0 +1,34 @@
+import './EntradaConTítulo.scss';
+import {ReactElement} from "react";
+import Título from "../../grupos-elementos-texto/título/Título.tsx";
+import {TipoDeEtiqueta} from "../../../../modelos/html/TipoDeEtiqueta.ts";
+import {
+    TipoDeEntrada
+} from "../../grupos-elementos-nativos/entrada/models/TipoDeEntrada.ts";
+import Entrada from "../../grupos-elementos-nativos/entrada/Entrada.tsx";
+
+interface PropsEntradaConTítulo {
+    textoDelTítulo: string;
+    tipoDeEntrada: TipoDeEntrada;
+    enLaEntrada: (valor: string | number) => void;
+    marcadorDePosición?: string;
+    min?: number;
+    max?: number;
+    valorPredeterminado?: number | string;
+}
+
+function EntradaConTítulo(props: PropsEntradaConTítulo): ReactElement {
+    return (
+        <div className={"entrada-con-título"}>
+            <Título texto={props.textoDelTítulo} tipoDeEtiqueta={TipoDeEtiqueta.H4}/>
+            <Entrada tipo={props.tipoDeEntrada}
+                     enLaEntrada={props.enLaEntrada}
+                     marcadorDePosición={props.marcadorDePosición}
+                     min={props.min}
+                     max={props.max}
+                     valorPredeterminado={props.valorPredeterminado}/>
+        </div>
+    );
+}
+
+export default EntradaConTítulo;
