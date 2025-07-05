2a8ce168ea307a70ada4d7bc3358aaa11ef0b4a9
theoliverlear <sigwartholiver2@gmail.com>
2025-07-05 10:14:04 -0500

    Edite BarraDeNav.tsx para agregar enlaces de navegación y el componente `AnclaDeCasa`

--- React/src/componentes/elementos/grupos-elementos-nav/barra-de-nav/BarraDeNav.tsx
+++ React/src/componentes/elementos/grupos-elementos-nav/barra-de-nav/BarraDeNav.tsx
@@ -1,12 +1,19 @@
 import type {ReactElement} from "react";
 import "./BarraDeNav.scss";
+import ElementoDeNav from "../elemento-de-nav/ElementoDeNav.tsx";
+import AnclaDeCasa from "../ancla-de-casa/AnclaDeCasa.tsx";
 
 function BarraDeNav(): ReactElement {
     return (
         <div className={"barra-de-nav"}>
-
+            <AnclaDeCasa/>
+            <nav>
+                <ElementoDeNav texto={"Panel"} enlace={"/panel"}/>
+                <ElementoDeNav texto={"Conjugador"} enlace={"/conj"}/>
+                <ElementoDeNav texto={"Perfil"} enlace={"/perfil"}/>
+            </nav>
         </div>
-    )
+    );
 }
 
 export default BarraDeNav;
