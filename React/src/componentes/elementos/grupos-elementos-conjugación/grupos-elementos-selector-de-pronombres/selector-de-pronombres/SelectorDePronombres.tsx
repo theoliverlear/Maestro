3267f100731c1c8f8f62e6c259dba910b018cd91
theoliverlear <sigwartholiver2@gmail.com>
2025-07-05 18:25:16 -0500

    Edite SelectorDePronombres.tsx para agregar estado, efectos e interfaz de propiedades en el manejo de pronombres seleccionados

--- React/src/componentes/elementos/grupos-elementos-conjugación/grupos-elementos-selector-de-pronombres/selector-de-pronombres/SelectorDePronombres.tsx
+++ React/src/componentes/elementos/grupos-elementos-conjugación/grupos-elementos-selector-de-pronombres/selector-de-pronombres/SelectorDePronombres.tsx
@@ -1,12 +1,37 @@
 import './SelectorDePronombres.scss';
-import {ReactElement} from "react";
-import {pronombres} from "../opción-de-pronombre/modelos/tipos.ts";
+import {ReactElement, useEffect, useState} from "react";
+import {Pronombre, pronombres} from "../opción-de-pronombre/modelos/tipos.ts";
 import OpciónDePronombre from "../opción-de-pronombre/OpciónDePronombre.tsx";
 
-function SelectorDePronombres(): ReactElement {
+interface PropsSelectorDePronombres {
+    enLaSelección: (pronombre: Pronombre) => void;
+}
+
+function SelectorDePronombres(props: PropsSelectorDePronombres): ReactElement {
+    const [pronombreSeleccionado, asignarPronombreSeleccionado] = useState<Pronombre>("Yo");
+    function selecciónDeMango(pronombre: Pronombre): void {
+        asignarPronombreSeleccionado(pronombre);
+        props.enLaSelección(pronombre);
+    }
+
+    function esComponenteSeleccionado(pronombreComponente: Pronombre): boolean {
+        return pronombreSeleccionado === pronombreComponente;
+    }
+
+    useEffect(() => {
+        pronombres.forEach((pronombre: Pronombre) => {
+            if (esComponenteSeleccionado(pronombre)) {
+                asignarPronombreSeleccionado(pronombre);
+            }
+        });
+    }, [pronombreSeleccionado]);
     return (
         <div className={"selector-de-pronombres"}>
-            {pronombres.map((pronombre: string, índice: number) => <OpciónDePronombre pronombre={pronombre} key={índice}/>)}
+            {pronombres.map((pronombre: string, índice: number) =>
+                <OpciónDePronombre pronombre={pronombre}
+                                   enSeleccionar={selecciónDeMango}
+                                   estáSeleccionado={esComponenteSeleccionado(pronombre)}
+                                   key={índice}/>)}
         </div>
     );
 }
