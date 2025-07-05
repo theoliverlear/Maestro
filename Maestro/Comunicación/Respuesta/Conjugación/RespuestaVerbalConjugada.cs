f9b4d36e7ec73e149b7d470981bea2e7e3dfa024
theoliverlear <sigwartholiver2@gmail.com>
2025-07-05 18:24:23 -0500

    Agregue RespuestaVerbalConjugada.cs para representar una respuesta de HTTP con un verbo conjugado

---
+++ Maestro/Comunicación/Respuesta/Conjugación/RespuestaVerbalConjugada.cs
@@ -0,0 +1,11 @@
+namespace Maestro.Comunicación.Respuesta.Conjugación;
+
+public class RespuestaVerbalConjugada
+{
+    public string VerboConjugado { get; }
+
+    public RespuestaVerbalConjugada(string verboConjugado)
+    {
+        this.VerboConjugado = verboConjugado;
+    }
+}
