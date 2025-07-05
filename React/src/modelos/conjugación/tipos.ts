837e1539f556e2d166f9c77e647f753d6e6d6bff
theoliverlear <sigwartholiver2@gmail.com>
2025-07-05 18:35:01 -0500

    Actualice tipos.ts para redefinir `Pronombre` como un tipo de unión y agregue la constante de cadena `pronombres`

--- React/src/modelos/conjugación/tipos.ts
+++ React/src/modelos/conjugación/tipos.ts
@@ -1,5 +1,6 @@
 export type Verbo = string;
-export type Pronombre = string;
+export const pronombres: string[] = ["Yo", "Tú", "Él", "Ella", "Nosotros", "Ellos", "Usted", "Ustedes"] as const;
+export type Pronombre = typeof pronombres[number];
 export type VerboConjugado = string;
 
 export type VerboConjugadoHttp = {
