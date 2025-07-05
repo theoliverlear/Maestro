7c6481d0ab85440f444f07bef22f6b400fdaffae
theoliverlear <sigwartholiver2@gmail.com>
2025-07-05 18:31:19 -0500

    Edite OpciónDePronombre.tsx para actualizar la ruta de importación de `Pronombre`

--- React/src/componentes/elementos/grupos-elementos-conjugación/grupos-elementos-selector-de-pronombres/opción-de-pronombre/OpciónDePronombre.tsx
+++ React/src/componentes/elementos/grupos-elementos-conjugación/grupos-elementos-selector-de-pronombres/opción-de-pronombre/OpciónDePronombre.tsx
@@ -1,8 +1,8 @@
 import './OpciónDePronombre.scss';
 import {ReactElement} from "react";
-import {Pronombre} from "./modelos/tipos.ts";
 import Título from "../../../grupos-elementos-texto/título/Título.tsx";
 import {TipoDeEtiqueta} from "../../../../../modelos/html/TipoDeEtiqueta.ts";
+import { Pronombre } from '../../../../../modelos/conjugación/tipos.ts';
 
 interface PropsOpciónDePronombre {
     pronombre: Pronombre;
