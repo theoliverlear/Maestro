940cdbddf14aa8897d69dbb959e581e52f262c53
theoliverlear <sigwartholiver2@gmail.com>
2025-07-05 10:12:11 -0500

    Edite App.tsx para renombrar `Conjugación` a `Conjugador` y mover `BarraDeNav` dentro del enrutador para consistencia estructural

--- React/src/componentes/app/App.tsx
+++ React/src/componentes/app/App.tsx
@@ -2,18 +2,18 @@ import './App.scss';
 import type {ReactElement} from "react";
 import {Route, BrowserRouter as Router, Routes} from "react-router-dom";
 import Casa from "../páginas/casa/Casa.tsx";
-import Conjugación from "../páginas/conjugación/Conjugación.tsx";
+import Conjugador from "../páginas/conjugador/Conjugador.tsx";
 import BarraDeNav
     from "../elementos/grupos-elementos-nav/barra-de-nav/BarraDeNav.tsx";
 
 function App(): ReactElement {
     return (
         <div className={"app"}>
-            <BarraDeNav/>
             <Router>
+                <BarraDeNav/>
                 <Routes>
                     <Route path={"/"} element={<Casa/>}/>
-                    <Route path={"/conj"} element={<Conjugación/>}/>
+                    <Route path={"/conj"} element={<Conjugador/>}/>
                 </Routes>
             </Router>
         </div>
