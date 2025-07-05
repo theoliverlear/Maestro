633c7db475a2ab12d8f0d9161afb473ff7f6c248
theoliverlear <sigwartholiver2@gmail.com>
2025-07-05 18:16:49 -0500

    Agregue ControladorDeConjugación.cs para manejar la conjugación de verbos mediante solicitudes HTTP

---
+++ Maestro/Controlador/ControladorDeConjugación.cs
@@ -0,0 +1,35 @@
+using Maestro.Comunicación.Respuesta.Conjugación;
+using Maestro.Excepción;
+using Maestro.Servicio.Conjugación;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Http;
+namespace Maestro.Controlador;
+
+[ApiController]
+[Route("api/conj")]
+public class ControladorDeConjugación : ControllerBase
+{
+    private IServicioDeConjugación _servicioDeConjugación;
+
+    public ControladorDeConjugación(
+        IServicioDeConjugación servicioDeConjugación)
+    {
+        this._servicioDeConjugación = servicioDeConjugación;
+    }
+
+    [HttpGet("{verbo}/{pronombre}")]
+    public ActionResult<RespuestaVerbalConjugada> Conjugar(string verbo, string pronombre)
+    {
+        string verboConjugado = string.Empty;
+        try
+        {
+            verboConjugado =
+                this._servicioDeConjugación.Conjugar(verbo, pronombre);
+        }
+        catch (ExcepciónDeVerboNoAdmitido ex)
+        {
+            verboConjugado = "Verbo no encontrado.";
+        }
+        return StatusCode(200, new RespuestaVerbalConjugada(verboConjugado));
+    }
+}
