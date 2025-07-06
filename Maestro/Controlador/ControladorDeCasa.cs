768b3fbc28dc4b926c1d3105c312ec894963378d
theoliverlear <sigwartholiver2@gmail.com>
2025-07-06 12:08:48 -0500

    Migrar la funcionalidad de prueba restante en ControladorDeCasa.cs a sus archivos apropiados

--- Maestro/Controlador/ControladorDeCasa.cs
+++ Maestro/Controlador/ControladorDeCasa.cs
@@ -1,6 +1,3 @@
-using Maestro.Excepción;
-using Maestro.Modelos.Ia;
-using Maestro.Servicio.Conjugación;
 using Microsoft.AspNetCore.Mvc;
 
 namespace Maestro.Controlador;
@@ -12,27 +9,5 @@ public class ControladorDeCasa : ControllerBase
     public ControladorDeCasa()
     {
 
-    }
-
-    private static readonly List<string> Alumnos =
-    [
-        "Oliver Sigwarth"
-    ];
-
-    [HttpGet]
-    public ActionResult<IEnumerable<string>> ObtenerTodo() => Ok(Alumnos);
-
-    [HttpGet("alumnos")]
-    public ActionResult<List<string>> ObtenerAlumnos()
-    {
-        return Ok(Alumnos);
-    }
-
-    [HttpGet("ia/palabras")]
-    public ActionResult<string> ObtenerPalabras()
-    {
-        MensajeDeIa mensaje = new MensajeDeIa();
-        string respuesta = mensaje.ObtenerListaDePalabras().Result;
-        return Ok(respuesta);
     }
 }
