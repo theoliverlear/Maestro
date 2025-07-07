cc2f6726127d812b4fc150308becc5fe78b4c857
theoliverlear <sigwartholiver2@gmail.com>
2025-07-07 10:37:05 -0500

    Edite Tarjeta.cs para extender `Identificable` y agregue un campo de respaldo privado `dificultad`

--- Maestro/Entidad/Tarjeta/Tarjeta.cs
+++ Maestro/Entidad/Tarjeta/Tarjeta.cs
@@ -4,11 +4,9 @@ using System.ComponentModel.DataAnnotations.Schema;
 namespace Maestro.Entidad.Tarjeta;
 
 [Table("tarjetas")]
-public class Tarjeta
-{
-    [Key]
-    public int Id { get; set; }
 
+public class Tarjeta : Identificable
+{
     [Column("en_español", TypeName = "varchar(100)")]
     public string EnEspañol { get; set; }
 
@@ -18,7 +16,7 @@ public class Tarjeta
     [Column("dificultad")]
     public int Dificultad
     {
-        get => this.Dificultad;
+        get => this._dificultad;
         set
         {
             if (value is < 1 or > 10)
@@ -26,8 +24,12 @@ public class Tarjeta
                 throw new ArgumentOutOfRangeException(nameof(value), mensajeDeDificultad);
             }
             this.Dificultad = value;
+            this._dificultad = value;
         }
     }
+
+    private int _dificultad;
+
     private static readonly string mensajeDeDificultad = "La dificultad debe" +
                                                          " estar entre 1 y 10.";
 
@@ -42,6 +44,7 @@ public class Tarjeta
         this.EnEspañol = string.Empty;
         this.EnInglés = string.Empty;
         this.Dificultad = 1;
+        this._dificultad = 1;
         this.BarajaDeCartasId = 0;
         this.BarajaDeCartas = new BarajaDeCartas();
     }
@@ -51,6 +54,7 @@ public class Tarjeta
         this.EnEspañol = enEspañol;
         this.EnInglés = enIngles;
         this.Dificultad = dificultad;
+        this._dificultad = dificultad;
         this.BarajaDeCartasId = 0;
         this.BarajaDeCartas = new BarajaDeCartas();
     }
@@ -62,6 +66,7 @@ public class Tarjeta
         this.EnEspañol = enEspañol;
         this.EnInglés = enIngles;
         this.Dificultad = dificultad;
+        this._dificultad = dificultad;
         this.BarajaDeCartasId = barajaDeCartas.Id;
         this.BarajaDeCartas = barajaDeCartas;
     }
