92b115fddf232374e1c00cd34492a7e1e3e6eb5d
theoliverlear <sigwartholiver2@gmail.com>
2025-07-06 19:19:00 -0500

    Edite Tarjeta.cs para agregar anotaciones de base de datos y propiedades adicionales

--- Maestro/Entidad/Tarjeta/Tarjeta.cs
+++ Maestro/Entidad/Tarjeta/Tarjeta.cs
@@ -1,46 +1,68 @@
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
 namespace Maestro.Entidad.Tarjeta;
 
-
+[Table("tarjetas")]
 public class Tarjeta
 {
-    private string enEspañol;
-    private string enIngles;
-    private int dificultad;
+    [Key]
+    public int Id { get; set; }
+
+    [Column("en_español", TypeName = "varchar(100)")]
+    public string EnEspañol { get; set; }
+
+    [Column("en_inglés", TypeName = "varchar(100)")]
+    public string EnInglés { get; set; }
+
+    [Column("dificultad")]
+    public int Dificultad
+    {
+        get => this.Dificultad;
+        set
+        {
+            if (value is < 1 or > 10)
+            {
+                throw new ArgumentOutOfRangeException(nameof(value), mensajeDeDificultad);
+            }
+            this.Dificultad = value;
+        }
+    }
     private static readonly string mensajeDeDificultad = "La dificultad debe" +
                                                          " estar entre 1 y 10.";
 
+    [Column("id_baraja_de_cartas")]
+    public int BarajaDeCartasId { get; set; }
+
+    [ForeignKey(nameof(BarajaDeCartasId))]
+    public BarajaDeCartas BarajaDeCartas { get; set; }
+
     public Tarjeta()
     {
-        this.enEspañol = string.Empty;
-        this.enIngles = string.Empty;
-        this.dificultad = -1;
+        this.EnEspañol = string.Empty;
+        this.EnInglés = string.Empty;
+        this.Dificultad = 1;
+        this.BarajaDeCartasId = 0;
+        this.BarajaDeCartas = new BarajaDeCartas();
     }
 
     public Tarjeta(string enEspañol, string enIngles, int dificultad)
     {
-        this.enEspañol = enEspañol;
-        this.enIngles = enIngles;
-        if (dificultad is < 1 or > 10)
-        {
-            throw new ArgumentOutOfRangeException(nameof(dificultad), mensajeDeDificultad);
-        }
-        this.dificultad = dificultad;
+        this.EnEspañol = enEspañol;
+        this.EnInglés = enIngles;
+        this.Dificultad = dificultad;
+        this.BarajaDeCartasId = 0;
+        this.BarajaDeCartas = new BarajaDeCartas();
     }
-    public long Id { get; }
-    public string EnEspañol { get; set; }
-
-    public string EnIngles { get; set; }
-
-    public int Dificultad
+    public Tarjeta(string enEspañol,
+                   string enIngles,
+                   int dificultad,
+                   BarajaDeCartas barajaDeCartas)
     {
-        get => this.dificultad;
-        set
-        {
-            if (value < 1 || value > 10)
-            {
-                throw new ArgumentOutOfRangeException(nameof(value), mensajeDeDificultad);
-            }
-            this.dificultad = value;
-        }
+        this.EnEspañol = enEspañol;
+        this.EnInglés = enIngles;
+        this.Dificultad = dificultad;
+        this.BarajaDeCartasId = barajaDeCartas.Id;
+        this.BarajaDeCartas = barajaDeCartas;
     }
 }
