0b1774d2268892a5525ff1ec1321cfbec5e8cb47
theoliverlear <sigwartholiver2@gmail.com>
2025-07-07 14:25:50 -0500

    Agregar funciones y constantes en recursosDeImagen.ts para manejar rutas de íconos

--- React/src/activos/recursosDeImagen.ts
+++ React/src/activos/recursosDeImagen.ts
@@ -4,6 +4,7 @@ export interface RecursoDeImagen {
 }
 
 const rutaAccesoDeImagen: string = "src/activos/imágenes/";
+const rutaAccesoDeImagenDelIcono: string = rutaAccesoDeImagen + "icono/"
 const rutaAccesoDeImagenDelLogotipo: string = rutaAccesoDeImagen + "logo/"
 
 export function obtenerRutaDeImagen(nombreDelArchivo: string): string {
@@ -12,6 +13,10 @@ export function obtenerRutaDeImagen(nombreDelArchivo: string): string {
 
 export function obtenerRutaDeImagenDelLogotipo(nombreDelArchivo: string): string {
     return rutaAccesoDeImagenDelLogotipo + nombreDelArchivo;
+}
+
+export function obtenerRutaDeImagenDelIcono(nombreDelArchivo: string): string {
+    return rutaAccesoDeImagenDelIcono + nombreDelArchivo;
 }
 
 export const recursoDeImagenDelLogotipo: RecursoDeImagen = {
@@ -23,3 +28,8 @@ export const recursoDeImagenDelLogotipoTransparente: RecursoDeImagen = {
     src: obtenerRutaDeImagenDelLogotipo('logotipo_transparente.png'),
     alt: 'Logotipo de aplicación transparente'
 };
+
+export const recursoDeImagenDeIconoDesplegable: RecursoDeImagen = {
+    src: obtenerRutaDeImagenDelIcono('icono_desplegable.svg'),
+    alt: 'Icono de menú desplegable'
+}
