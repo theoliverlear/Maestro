84caa999e14463b28e52a9fb0989503a09a18c68
theoliverlear <sigwartholiver2@gmail.com>
2025-07-07 10:26:42 -0500

    Edite ContraseñaSegura.cs para agregar anotaciones de base de datos, extender `Identificable`, y definir relaciones con la entidad Usuario

--- Maestro/Entidad/Usuario/ContraseñaSegura.cs
+++ Maestro/Entidad/Usuario/ContraseñaSegura.cs
@@ -1,26 +1,44 @@
-﻿using Microsoft.AspNetCore.Identity;
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using Microsoft.AspNetCore.Identity;
 
 using Microsoft.AspNetCore.Identity;
 namespace Maestro.Entidad.Usuario;
 
-public class ContraseñaSegura
+[Table("contraseñas_seguros")]
+public class ContraseñaSegura : Identificable
 {
-    private long id;
-    private string contraseñaCodificada;
+    [Key, Column("id"), ForeignKey(nameof(Usuario))]
+    public int Id { get; set; }
+
+    public Usuario Usuario { get; set; }
+
+    [Column("contraseña_codificada", TypeName = "varchar(100)")]
+    public string ContraseñaCodificada { get; set; }
+
+    public ContraseñaSegura()
+    {
+        this.ContraseñaCodificada = string.Empty;
+        this.Usuario = new Usuario();
+    }
 
     public ContraseñaSegura(string contraseñaSinCodificar)
     {
-        this.contraseñaCodificada = this.Codificar(contraseñaSinCodificar);
+        this.ContraseñaCodificada = this.Codificar(contraseñaSinCodificar);
+        this.Usuario = new Usuario();
+    }
+
+    public ContraseñaSegura(string contraseñaSinCodificar, Usuario usuario)
+    {
+        this.ContraseñaCodificada = this.Codificar(contraseñaSinCodificar);
+        this.Usuario = usuario;
+        this.Id = usuario.Id;
+        this.Usuario.IdContraseñaSegura = this.Id;
     }
 
     public string Codificar(string contraseñaSinCodificar)
     {
         PasswordHasher<ContraseñaSegura> picadora = new PasswordHasher<ContraseñaSegura>();
         return picadora.HashPassword(this, contraseñaSinCodificar);
-    }
-
-    public string ContraseñaCodificada
-    {
-        get; set;
     }
 }
