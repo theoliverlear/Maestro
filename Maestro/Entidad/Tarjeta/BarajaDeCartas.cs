c0a42688d3456b54447eed26824a99cef1de94a4
theoliverlear <sigwartholiver2@gmail.com>
2025-07-06 19:10:03 -0500

    Edite BarajaDeCartas.cs a definir la clase con relaciones de usuario, cartas, y anotaciones para la base de datos

--- Maestro/Entidad/Tarjeta/BarajaDeCartas.cs
+++ Maestro/Entidad/Tarjeta/BarajaDeCartas.cs
@@ -1,6 +1,34 @@
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
 namespace Maestro.Entidad.Tarjeta;
 
+using Maestro.Entidad.Usuario;
+
+[Table("barajas_de_cartas")]
 public class BarajaDeCartas
 {
-    private List<Tarjeta> cartas;
+    [Key, ForeignKey(nameof(Usuario))]
+    public int Id { get; set; }
+
+    [InverseProperty(nameof(BarajaDeCartas))]
+    public ICollection<Tarjeta> Cartas { get; set; }
+
+    public Usuario Usuario { get; set; }
+
+    public BarajaDeCartas() : this(new List<Tarjeta>())
+    {
+
+    }
+
+    public BarajaDeCartas(List<Tarjeta> cartas) : this(new Usuario(), cartas)
+    {
+
+    }
+
+    public BarajaDeCartas(Usuario usuario, List<Tarjeta> cartas)
+    {
+        this.Usuario = usuario;
+        this.Cartas = cartas;
+    }
 }
