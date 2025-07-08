e6a599324936b9e8e8d54a58c3cf205e629a0d6f
theoliverlear <sigwartholiver2@gmail.com>
2025-07-07 19:03:23 -0500

    Expandir IServicioDeConjugación.cs para incluir sobrecargas del método `Conjugar()` con parámetros de ánimo y tenso

--- Maestro/Servicio/Conjugación/ServicioDeConjugación/IServicioDeConjugación.cs
+++ Maestro/Servicio/Conjugación/ServicioDeConjugación/IServicioDeConjugación.cs
@@ -6,4 +6,6 @@ public interface IServicioDeConjugación
 {
     string Conjugar(string verbo, Pronombre pronombre);
     string Conjugar(string verbo, string pronombre);
+    string Conjugar(string verbo, string pronombre, string ánimo);
+    string Conjugar(string verbo, string pronombre, string ánimo, string tenso);
 }
