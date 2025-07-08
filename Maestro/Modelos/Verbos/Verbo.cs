bc2fc59afdbcffc1728ea9beaf81005a457f9769
theoliverlear <sigwartholiver2@gmail.com>
2025-07-07 19:15:17 -0500

    Refactorizar Verbo.cs para simplificar la lógica de conjugación utilizando la nueva funcionalidad de conjugación de patrones de construcción de `Conjugador`

--- Maestro/Modelos/Verbos/Verbo.cs
+++ Maestro/Modelos/Verbos/Verbo.cs
@@ -21,30 +21,7 @@ public class Verbo
 
     public string Conjugado(Pronombre pronombre)
     {
-        if (pronombre.EsYo())
-        {
-            return Conjugador.ObtenerYo(this.verbo);
-        }
-
-        if (pronombre.EsTú())
-        {
-            return Conjugador.ObtenerTú(this.verbo);
-        }
-
-        if (pronombre.EsElla() || pronombre.EsÉl() || pronombre.EsUsted())
-        {
-            return Conjugador.ObtenerÉl(this.verbo);
-        }
-
-        if (pronombre.EsUstedes() || pronombre.EsEllos())
-        {
-            return Conjugador.ObtenerEllos(this.verbo);
-        }
-
-        if (pronombre.EsNosotros())
-        {
-            return Conjugador.ObtenerNosotros(this.verbo);
-        }
-        return string.Empty;
+        return Conjugador.ConjugarConVerboPronombre(this.verbo,
+                                                    pronombre.Sujeto);
     }
 }
