652289bda45997193af0da0938c219af5d04b955
theoliverlear <sigwartholiver2@gmail.com>
2025-07-08 08:16:50 -0500

    Agregue BarajaDeCartas.tsx para renderizar el componente de página `BarajaDeCartas` en la aplicación React

---
+++ React/src/componentes/páginas/baraja-de-cartas/BarajaDeCartas.tsx
@@ -0,0 +1,12 @@
+import './BarajaDeCartas.scss';
+import {ReactElement} from "react";
+
+function BarajaDeCartas(): ReactElement {
+    return (
+        <div className={"baraja-de-cartas"}>
+
+        </div>
+    );
+}
+
+export default BarajaDeCartas;
