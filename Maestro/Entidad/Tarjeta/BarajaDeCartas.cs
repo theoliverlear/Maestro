49326a469213903f971a5b3e0cb6ae694ac77fba
theoliverlear <sigwartholiver2@gmail.com>
2025-07-08 14:13:06 -0500

    Refactorizar BarajaDeCartas.cs para agregar atributo `Título`, enlace de usuario, y nuevos constructores

--- Maestro/Entidad/Tarjeta/BarajaDeCartas.cs
+++ Maestro/Entidad/Tarjeta/BarajaDeCartas.cs
@@ -8,13 +8,22 @@ using Maestro.Entidad.Usuario;
 [Table("barajas_de_cartas")]
 public class BarajaDeCartas : Identificable
 {
-    [Key, Column("id"), ForeignKey(nameof(Usuario))]
-    public int Id { get; set; }
-
     [InverseProperty(nameof(BarajaDeCartas))]
     public ICollection<Tarjeta> Cartas { get; set; }
 
+    [Column("título", TypeName = "varchar(100)")]
+    public string Título { get; set; }
+
+    [Column("id_usuario")]
+    public int IdUsuario { get; set; }
+
+    [ForeignKey(nameof(IdUsuario))]
     public Usuario Usuario { get; set; }
+
+    public BarajaDeCartas(string título) : this(new Usuario(), new List<Tarjeta>(), título)
+    {
+        this.Título = título;
+    }
 
     public BarajaDeCartas() : this(new List<Tarjeta>())
     {
@@ -30,5 +39,12 @@ public class BarajaDeCartas : Identificable
     {
         this.Usuario = usuario;
         this.Cartas = cartas;
+    }
+
+    public BarajaDeCartas(Usuario usuario, ICollection<Tarjeta> cartas, string título)
+    {
+        this.Usuario = usuario;
+        this.Cartas = cartas;
+        this.Título = título;
     }
 }
