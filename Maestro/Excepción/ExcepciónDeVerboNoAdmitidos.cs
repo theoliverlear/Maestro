b3afd03efbc89294df7d7886324c64ff965494d0
theoliverlear <sigwartholiver2@gmail.com>
2025-07-07 19:01:18 -0500

    Refactorizar ExcepciónDeVerboNoAdmitido.cs para corregir el nombre de la clase y constructor a `ExcepciónDeVerboNoAdmitidos`

--- Maestro/Excepción/ExcepciónDeVerboNoAdmitidos.cs
+++ Maestro/Excepción/ExcepciónDeVerboNoAdmitidos.cs
@@ -1,11 +1,11 @@
 namespace Maestro.Excepción;
 
-public class ExcepciónDeVerboNoAdmitido : Exception
+public class ExcepciónDeVerboNoAdmitidos : Exception
 {
     public static string Mensaje = "Este verbo no está soportado en el" +
                                    " diccionario de Maestro: ";
 
-    public ExcepciónDeVerboNoAdmitido(string verbo) : base(Mensaje + verbo)
+    public ExcepciónDeVerboNoAdmitidos(string verbo) : base(Mensaje + verbo)
     {
 
     }
