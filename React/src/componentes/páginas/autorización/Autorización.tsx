1941be209f0ef2bfdeeb7cd78c743efe74202a82
theoliverlear <sigwartholiver2@gmail.com>
2025-07-09 15:04:36 -0500

    Agregue Autorización.tsx para definir el componente de página `Autorización` en React para iniciar sesión y registrarse

---
+++ React/src/componentes/páginas/autorización/Autorización.tsx
@@ -0,0 +1,12 @@
+import './Autorización.scss';
+import {ReactElement} from "react";
+
+function Autorización(): ReactElement {
+    return (
+        <div className={"autorización"}>
+
+        </div>
+    );
+}
+
+export default Autorización;
