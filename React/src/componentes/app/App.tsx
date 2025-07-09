9288d863bdbe41b7cf53906936a37cf1c5b4ea94
theoliverlear <sigwartholiver2@gmail.com>
2025-07-09 14:36:51 -0500

    Edite App.tsx para agregar la ruta "/cuenta" y vincularla al componente `AnclaDeCuenta`

--- React/src/componentes/app/App.tsx
+++ React/src/componentes/app/App.tsx
@@ -6,6 +6,8 @@ import Conjugador from "../páginas/conjugador/Conjugador.tsx";
 import BarraDeNav
     from "../elementos/grupos-elementos-nav/barra-de-nav/BarraDeNav.tsx";
 import BarajaDeCartas from "../páginas/baraja-de-cartas/BarajaDeCartas.tsx";
+import AnclaDeCuenta
+    from "../elementos/grupos-elementos-nav/ancla-de-cuenta/AnclaDeCuenta.tsx";
 
 function App(): ReactElement {
     return (
@@ -16,6 +18,7 @@ function App(): ReactElement {
                     <Route path={"/"} element={<Casa/>}/>
                     <Route path={"/conj"} element={<Conjugador/>}/>
                     <Route path={"/baraja-de-cartas"} element={<BarajaDeCartas/>}/>
+                    <Route path={"/cuenta"} element={<AnclaDeCuenta/>}/>
                 </Routes>
             </Router>
         </div>
