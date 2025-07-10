5cf43fd1f41b71be86482e2c958adb108f1b5807
theoliverlear <sigwartholiver2@gmail.com>
2025-07-10 18:11:34 -0500

    Agregue constructor y propiedad `EsAutorizado` a RespuestaDeEstadoDeAutorización.cs para inicializar el estado de autorización

--- Maestro/Comunicación/Respuesta/Autorización/RespuestaDeEstadoDeAutorización.cs
+++ Maestro/Comunicación/Respuesta/Autorización/RespuestaDeEstadoDeAutorización.cs
@@ -1,6 +1,12 @@
+using Maestro.Modelos.Autorización;
+
 namespace Maestro.Comunicación.Respuesta.Autorización;
 
 public class RespuestaDeEstadoDeAutorización
 {
-    
+    public bool EsAutorizado { get; private set; }
+    public RespuestaDeEstadoDeAutorización(EstadoDeAutorización estado)
+    {
+        this.EsAutorizado = estado.EsAutorizado;
+    }
 }
