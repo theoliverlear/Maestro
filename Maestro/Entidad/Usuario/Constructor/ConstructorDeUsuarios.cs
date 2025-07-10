3221a84619391bbb65dc442c755ec6fae3b6b06c
theoliverlear <sigwartholiver2@gmail.com>
2025-07-10 18:02:53 -0500

    Agregue ConstructorDeUsuarios.cs para construir instancias de usuario con un patrón de diseño de constructor

---
+++ Maestro/Entidad/Usuario/Constructor/ConstructorDeUsuarios.cs
@@ -0,0 +1,56 @@
+using Maestro.Entidad.Tarjeta;
+using Maestro.Entidad.Usuario.Constructor.Modelos;
+
+namespace Maestro.Entidad.Usuario.Constructor;
+
+public class ConstructorDeUsuarios : UsuarioAbstracto
+{
+    private string _nombre;
+    private string _correoElectrónico;
+    private ICollection<BarajaDeCartas> _barajasDeCartas;
+    private ContraseñaSegura _contraseñaSegura;
+    public ConstructorDeUsuarios()
+    {
+        this._nombre = string.Empty;
+        this._correoElectrónico = string.Empty;
+        this._barajasDeCartas = new List<BarajaDeCartas>();
+        this._contraseñaSegura = new ContraseñaSegura(string.Empty);
+    }
+
+
+    public override UsuarioAbstracto ConNombre(string nombre)
+    {
+        this._nombre = nombre;
+        return this;
+    }
+
+    public override UsuarioAbstracto ConCorreoElectrónico(string correoElectrónico)
+    {
+        this._correoElectrónico = correoElectrónico;
+        return this;
+    }
+
+    public override UsuarioAbstracto ConBarajasDeCartas(ICollection<BarajaDeCartas> barajasDeCartas)
+    {
+        this._barajasDeCartas = barajasDeCartas;
+        return this;
+    }
+
+    public override UsuarioAbstracto ConContraseñaSegura(ContraseñaSegura contraseñaSegura)
+    {
+        this._contraseñaSegura = contraseñaSegura;
+        return this;
+    }
+
+    public override Usuario Construir()
+    {
+        var usuarioNuevo = new Usuario(
+            this._nombre,
+            this._correoElectrónico,
+            this._barajasDeCartas,
+            this._contraseñaSegura
+        );
+        this._contraseñaSegura.Usuario = usuarioNuevo;
+        return usuarioNuevo;
+    }
+}
