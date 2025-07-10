757e60df21adda028d2f4d83857457819cffc0ac
theoliverlear <sigwartholiver2@gmail.com>
2025-07-10 18:06:06 -0500

    Agregar dependencias de servicios de sesión y usuario en ServicioDeAutorización.cs

--- Maestro/Servicio/Autorización/ServicioDeAutorización/ServicioDeAutorización.cs
+++ Maestro/Servicio/Autorización/ServicioDeAutorización/ServicioDeAutorización.cs
@@ -1,10 +1,21 @@
 using Maestro.Comunicación.Respuesta.Autorización;
 using Maestro.Comunicación.Solicitud.Autorización;
+using Maestro.Servicio.Sesión.ServicioDeSesión;
+using Maestro.Servicio.Usuarios.ServicioDeUsuario;
 
 namespace Maestro.Servicio.Autorización.ServicioDeAutorización;
 
 public class ServicioDeAutorización : IServicioDeAutorización
 {
+    private readonly IServicioDeSesión _servicioDeSesión;
+    private readonly IServicioDeUsuario _servicioDeUsuario;
+    public ServicioDeAutorización(IServicioDeSesión servicioDeSesión,
+                                  IServicioDeUsuario servicioDeUsuario)
+    {
+        this._servicioDeSesión = servicioDeSesión;
+        this._servicioDeUsuario = servicioDeUsuario;
+    }
+
     public RespuestaDeEstadoDeAutorización Acceso(SolicitudInicioDeSesión solicitud)
     {
         throw new NotImplementedException();
