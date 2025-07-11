ef4d0d24b15560817de8f2d33d4a3520f72b8861
theoliverlear <sigwartholiver2@gmail.com>
2025-07-11 18:55:46 -0500

    Agregue métodos `ConContraseñaSegura()` y `ConNombreDeUsuario()` en UsuarioAbstracto.cs para configurar contraseña segura y nombre de usuario durante la construcción de instancias

--- Maestro/Entidad/Usuario/Constructor/Modelos/UsuarioAbstracto.cs
+++ Maestro/Entidad/Usuario/Constructor/Modelos/UsuarioAbstracto.cs
@@ -9,5 +9,7 @@ public abstract class UsuarioAbstracto : IFábricaDeConstrucción<Usuario>
     public abstract UsuarioAbstracto ConCorreoElectrónico(string correoElectrónico);
     public abstract UsuarioAbstracto ConBarajasDeCartas(ICollection<BarajaDeCartas> barajasDeCartas);
     public abstract UsuarioAbstracto ConContraseñaSegura(ContraseñaSegura contraseñaSegura);
+    public abstract UsuarioAbstracto ConContraseñaSegura(string contraseñaSegura);
+    public abstract UsuarioAbstracto ConNombreDeUsuario(string nombreDeUsuario);
     public abstract Usuario Construir();
 }
