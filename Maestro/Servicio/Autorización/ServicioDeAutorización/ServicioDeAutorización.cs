8e97bba23aaeed8defeb6a17917f2a009f2762e5
theoliverlear <sigwartholiver2@gmail.com>
2025-07-11 18:57:55 -0500

    Implemente el método `Acceso()` en ServicioDeAutorización.cs para manejar la autenticación verificando estado de sesión, existencia de usuario y contraseñas coincidentes

--- Maestro/Servicio/Autorización/ServicioDeAutorización/ServicioDeAutorización.cs
+++ Maestro/Servicio/Autorización/ServicioDeAutorización/ServicioDeAutorización.cs
@@ -1,5 +1,7 @@
 using Maestro.Comunicación.Respuesta.Autorización;
 using Maestro.Comunicación.Solicitud.Autorización;
+using Maestro.Entidad.Usuario;
+using Maestro.Modelos.Autorización;
 using Maestro.Servicio.Sesión.ServicioDeSesión;
 using Maestro.Servicio.Usuarios.ServicioDeUsuario;
 
@@ -18,7 +20,18 @@ public class ServicioDeAutorización : IServicioDeAutorización
 
     public RespuestaDeEstadoDeAutorización Acceso(SolicitudInicioDeSesión solicitud)
     {
-        throw new NotImplementedException();
+        if (this._servicioDeSesión.UsuarioEnSesión())
+        {
+            return new(EstadoDeAutorización.Autoizado);
+        }
+        Usuario? usuario = this._servicioDeUsuario.ObtenerPorNombreDeUsuario(solicitud.NombreDeUsuario);
+        if (usuario == null)
+        {
+            return new(EstadoDeAutorización.NoAutorizado);
+        }
+        bool contraseñasCoinciden = usuario.ContraseñaSegura.CoincidenciasCodificadas(solicitud.Contraseña);
+        EstadoDeAutorización estadoDeAutorización = EstadoDeAutorización.DelEstadoDeContraseña(contraseñasCoinciden);
+        return new(estadoDeAutorización);
     }
 
     public RespuestaDeEstadoDeAutorización Registro(SolicitudDeRegistro solicitud)
