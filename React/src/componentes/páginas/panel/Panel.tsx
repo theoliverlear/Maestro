7f33222d13c132afc3570c59b26ae1470a3f3ba0
theoliverlear <sigwartholiver2@gmail.com>
2025-07-12 14:08:54 -0500

    Agregue Panel.tsx como un componente React para la página del panel de usuario

---
+++ React/src/componentes/páginas/panel/Panel.tsx
@@ -0,0 +1,12 @@
+import "./Panel.scss";
+import {ReactElement} from "react";
+
+function Panel(): ReactElement {
+    return (
+        <div className={"panel"}>
+
+        </div>
+    );
+}
+
+export default Panel;
