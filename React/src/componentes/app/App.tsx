d71f7e1a4904f840be4914a086b768daeb4b62ca
theoliverlear <sigwartholiver2@gmail.com>
2025-07-12 13:49:40 -0500

    Agregue contexto de autorización y rutas protegidas con `AutorizaciónRutaProtegida` en App.tsx para gestionar acceso a componentes restringidos

--- React/src/componentes/app/App.tsx
+++ React/src/componentes/app/App.tsx
@@ -1,3 +1,7 @@
+// App.tsx
+import {
+    ProveedorDeAutorización
+} from "../contextos/contexto-de-autorización/ContextoDeAutorización.tsx";
 import './App.scss';
 import type {ReactElement} from "react";
 import {Route, BrowserRouter as Router, Routes} from "react-router-dom";
@@ -8,18 +12,34 @@ import BarraDeNav
 import BarajaDeCartas from "../páginas/baraja-de-cartas/BarajaDeCartas.tsx";
 import AnclaDeCuenta
     from "../elementos/grupos-elementos-nav/ancla-de-cuenta/AnclaDeCuenta.tsx";
+import Autorización from "../páginas/autorización/Autorización.tsx";
+import AutorizaciónRutaProtegida
+    from "../elementos/grupos-elementos-autorización/autorización-ruta-protegida/AutorizaciónRutaProtegida.tsx";
+import Panel from "../páginas/panel/Panel.tsx";
 
 function App(): ReactElement {
     return (
         <div className={"app"}>
             <Router>
-                <BarraDeNav/>
-                <Routes>
-                    <Route path={"/"} element={<Casa/>}/>
-                    <Route path={"/conj"} element={<Conjugador/>}/>
-                    <Route path={"/baraja-de-cartas"} element={<BarajaDeCartas/>}/>
-                    <Route path={"/cuenta"} element={<AnclaDeCuenta/>}/>
-                </Routes>
+                <ProveedorDeAutorización>
+                    <BarraDeNav/>
+                    <Routes>
+                        <Route path={"/"} element={<Casa/>}/>
+                        <Route path={"/conj"} element={<Conjugador/>}/>
+                        <Route path={"/baraja-de-cartas"} element={<BarajaDeCartas/>}/>
+                        <Route path={"/cuenta"} element={
+                            <AutorizaciónRutaProtegida>
+                                <AnclaDeCuenta/>
+                            </AutorizaciónRutaProtegida>
+                        }/>
+                        <Route path={"/autorización"} element={<Autorización/>}/>
+                        <Route path={"/panel"} element={
+                            <AutorizaciónRutaProtegida>
+                                <Panel/>
+                            </AutorizaciónRutaProtegida>
+                        }/>
+                    </Routes>
+                </ProveedorDeAutorización>
             </Router>
         </div>
     );
