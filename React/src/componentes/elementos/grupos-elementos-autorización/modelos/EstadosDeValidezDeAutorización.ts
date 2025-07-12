09a8f7563e995b39139a0fa3ee8aa5dc2b9e9d69
theoliverlear <sigwartholiver2@gmail.com>
2025-07-12 14:02:05 -0500

    Agregue EstadosDeValidezDeAutorización.ts para enumerar los posibles estados de validación de autorización

---
+++ React/src/componentes/elementos/grupos-elementos-autorización/modelos/EstadosDeValidezDeAutorización.ts
@@ -0,0 +1,6 @@
+export enum EstadosDeValidezDeAutorización {
+    VÁLIDO = "Válido.",
+    FALTA_DE_COINCIDENCIA_DE_CONTRASEÑAS = "Falta de coincidencia de contraseñas.",
+    CORREO_ELECTRÓNICO_NO_VÁLIDO = "Correo electrónico no válido.",
+    CAMPOS_SIN_RELLENAR = "Campos sin rellenar.",
+}
