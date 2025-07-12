eceaf68691a854cfb6d40ec4eeb50dde027872a8
theoliverlear <sigwartholiver2@gmail.com>
2025-07-12 13:57:17 -0500

    Agregue ConsolaDeInicioDeSesión.tsx como componente React para la consola de inicio de sesión

---
+++ React/src/componentes/elementos/grupos-elementos-autorización/consola-de-inicio-de-sesión/ConsolaDeInicioDeSesión.tsx
@@ -0,0 +1,12 @@
+import './ConsolaDeInicioDeSesión.scss';
+import {ReactElement} from "react";
+
+function ConsolaDeInicioDeSesión(): ReactElement {
+    return (
+        <div className={"consola-de-inicio-de-sesión"}>
+
+        </div>
+    );
+}
+
+export default ConsolaDeInicioDeSesión;
