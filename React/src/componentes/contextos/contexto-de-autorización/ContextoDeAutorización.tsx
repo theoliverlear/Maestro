224a99b4cab3c52199d410255d8d5066819e80c0
theoliverlear <sigwartholiver2@gmail.com>
2025-07-12 13:59:27 -0500

    Implementar ContextoDeAutorización.tsx como un contexto de React para gestionar la autorización y el estado de acceso

--- React/src/componentes/contextos/contexto-de-autorización/ContextoDeAutorización.tsx
+++ React/src/componentes/contextos/contexto-de-autorización/ContextoDeAutorización.tsx
@@ -1,1 +1,51 @@
-// TODO: Implementar un protector de cuenta usando contextos de React.
+// ContextoDeAutorización.tsx
+
+import {
+    createContext,
+    ReactNode,
+    useContext,
+    useEffect,
+    useMemo
+} from "react";
+import {PosibleIContextoDeAutorización} from "./modelos/tipos.ts";
+import {usarConectado} from "../../../manos/usarConectado.ts";
+
+export interface IContextoDeAutorización {
+    conectado: boolean;
+    refrescar: () => Promise<void>;
+}
+
+const ContextoDeAutorización = createContext<PosibleIContextoDeAutorización>(null);
+
+export function ProveedorDeAutorización({ children }: { children: ReactNode }) {
+    const {
+        conectado,
+        estáAutorizado
+    } = usarConectado();
+
+    useEffect(() => {
+        (async () => {
+           await estáAutorizado();
+        })();
+    }, []);
+
+    const valor = useMemo(() => ({ conectado, refrescar: estáAutorizado }),
+        [conectado, estáAutorizado]);
+    
+    return (
+        <ContextoDeAutorización.Provider value={valor}>
+            {children}
+        </ContextoDeAutorización.Provider>
+    );
+}
+
+export function usarAutorización() {
+    const contexto: PosibleIContextoDeAutorización = useContext(ContextoDeAutorización);
+    if (!contexto) {
+        throw new Error("El contexto de autorización no está disponible. " +
+                        "Asegúrate de envolver tu componente con " +
+                        "ProveedorDeAutorización.");
+    }
+    return contexto;
+}
+
