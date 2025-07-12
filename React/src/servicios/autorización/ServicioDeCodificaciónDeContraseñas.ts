e7e1db52c1c3773ad41dd7b1e47feae7a5f9f5b0
theoliverlear <sigwartholiver2@gmail.com>
2025-07-12 14:11:24 -0500

    Agregue ServicioDeCodificaciónDeContraseñas.ts para codificar contraseñas usando SHA256 con CryptoJS

---
+++ React/src/servicios/autorización/ServicioDeCodificaciónDeContraseñas.ts
@@ -0,0 +1,9 @@
+import {injectable} from "tsyringe";
+import CryptoJS from 'crypto-js';
+
+@injectable()
+export class ServicioDeCodificaciónDeContraseñas {
+    public codificar(contraseña: string): string {
+        return CryptoJS.SHA256(contraseña).toString();
+    }
+}
