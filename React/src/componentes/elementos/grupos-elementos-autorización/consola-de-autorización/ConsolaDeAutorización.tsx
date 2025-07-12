8f2e38ab110ff951357837712bbcc52785aacb4b
theoliverlear <sigwartholiver2@gmail.com>
2025-07-12 13:54:07 -0500

    Agregue ConsolaDeAutorización.tsx como componente React para la consola de autorización

---
+++ React/src/componentes/elementos/grupos-elementos-autorización/consola-de-autorización/ConsolaDeAutorización.tsx
@@ -0,0 +1,12 @@
+import './ConsolaDeAutorización.scss';
+import {ReactElement} from "react";
+
+function ConsolaDeAutorización(): ReactElement {
+    return (
+        <div className={"consola-de-autorización"}>
+
+        </div>
+    );
+}
+
+export default ConsolaDeAutorización;
