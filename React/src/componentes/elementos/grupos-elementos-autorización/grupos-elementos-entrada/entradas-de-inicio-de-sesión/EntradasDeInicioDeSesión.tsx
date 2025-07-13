ef35a628580d9269fdeae949833ce305fc8b80cf
theoliverlear <sigwartholiver2@gmail.com>
2025-07-13 14:26:52 -0500

    Agregue EntradasDeInicioDeSesión.tsx como un componente React para capturar la información de inicio de sesión del usuario

---
+++ React/src/componentes/elementos/grupos-elementos-autorización/grupos-elementos-entrada/entradas-de-inicio-de-sesión/EntradasDeInicioDeSesión.tsx
@@ -0,0 +1,12 @@
+import './EntradasDeInicioDeSesión.scss';
+import {ReactElement} from "react";
+
+function EntradasDeInicioDeSesión(): ReactElement {
+    return (
+        <div className={"entradas-de-inicio-de-sesión"}>
+
+        </div>
+    );
+}
+
+export default EntradasDeInicioDeSesión;
