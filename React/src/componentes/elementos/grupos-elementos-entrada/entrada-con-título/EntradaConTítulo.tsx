50b8d00dc24e10ba2b7bc52df8a7c840bf9c1d39
theoliverlear <sigwartholiver2@gmail.com>
2025-07-13 14:23:52 -0500

    Edite EntradaConTítulo.tsx para agregar soporte opcional para el tipo de etiqueta del título mediante la función `obtenerTipoDeEtiqueta()`

--- React/src/componentes/elementos/grupos-elementos-entrada/entrada-con-título/EntradaConTítulo.tsx
+++ React/src/componentes/elementos/grupos-elementos-entrada/entrada-con-título/EntradaConTítulo.tsx
@@ -11,6 +11,7 @@ interface PropsEntradaConTítulo {
     textoDelTítulo: string;
     tipoDeEntrada: TipoDeEntrada;
     enLaEntrada: (valor: string | number) => void;
+    tipoDeEtiqueta?: TipoDeEtiqueta;
     normalizarEntrada?: (valor: string | number) => string | number;
     marcadorDePosición?: string;
     min?: number;
@@ -19,9 +20,14 @@ interface PropsEntradaConTítulo {
 }
 
 function EntradaConTítulo(props: PropsEntradaConTítulo): ReactElement {
+
+    function obtenerTipoDeEtiqueta(): TipoDeEtiqueta {
+        return props.tipoDeEtiqueta ? props.tipoDeEtiqueta : TipoDeEtiqueta.H3;
+    }
+
     return (
         <div className={"entrada-con-título"}>
-            <Título texto={props.textoDelTítulo} tipoDeEtiqueta={TipoDeEtiqueta.H4}/>
+            <Título texto={props.textoDelTítulo} tipoDeEtiqueta={obtenerTipoDeEtiqueta()}/>
             <Entrada tipo={props.tipoDeEntrada}
                      enLaEntrada={props.enLaEntrada}
                      marcadorDePosición={props.marcadorDePosición}
