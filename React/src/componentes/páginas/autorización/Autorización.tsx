6844a1d103cc96621ef7b6dc7e1dbaf500691299
theoliverlear <sigwartholiver2@gmail.com>
2025-07-13 14:12:56 -0500

    Edite Autorización.tsx para integrar el componente `ConsolaDeAutorización` en la página de autorización

--- React/src/componentes/páginas/autorización/Autorización.tsx
+++ React/src/componentes/páginas/autorización/Autorización.tsx
@@ -1,10 +1,12 @@
 import './Autorización.scss';
 import {ReactElement} from "react";
+import ConsolaDeAutorización
+    from "../../elementos/grupos-elementos-autorización/consola-de-autorización/ConsolaDeAutorización.tsx";
 
 function Autorización(): ReactElement {
     return (
         <div className={"autorización"}>
-
+            <ConsolaDeAutorización/>
         </div>
     );
 }
