e1888cb9bec32f0d6249a571791b78c126004c5d
theoliverlear <sigwartholiver2@gmail.com>
2025-07-13 14:21:48 -0500

    Agregar el componente `EntradasDeRegistro` a ConsolaDeRegistro.tsx para la funcionalidad de entrada de registro

--- React/src/componentes/elementos/grupos-elementos-autorización/consola-de-registro/ConsolaDeRegistro.tsx
+++ React/src/componentes/elementos/grupos-elementos-autorización/consola-de-registro/ConsolaDeRegistro.tsx
@@ -1,10 +1,12 @@
 import './ConsolaDeRegistro.scss';
 import {ReactElement} from "react";
+import EntradasDeRegistro
+    from "../grupos-elementos-entrada/entradas-de-registro/EntradasDeRegistro.tsx";
 
 function ConsolaDeRegistro(): ReactElement {
     return (
         <div className={"consola-de-registro"}>
-
+            <EntradasDeRegistro/>
         </div>
     );
 }
