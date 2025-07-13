ad6abf238152b179d346eceb6ed9e18079ff3eac
theoliverlear <sigwartholiver2@gmail.com>
2025-07-13 14:19:15 -0500

    Refactorizar ConsolaDeAutorización.tsx para integrar los componentes `ConsolaDeInicioDeSesión` y `ConsolaDeRegistro`

--- React/src/componentes/elementos/grupos-elementos-autorización/consola-de-autorización/ConsolaDeAutorización.tsx
+++ React/src/componentes/elementos/grupos-elementos-autorización/consola-de-autorización/ConsolaDeAutorización.tsx
@@ -1,10 +1,14 @@
 import './ConsolaDeAutorización.scss';
 import {ReactElement} from "react";
+import ConsolaDeInicioDeSesión
+    from "../consola-de-inicio-de-sesión/ConsolaDeInicioDeSesión.tsx";
+import ConsolaDeRegistro from "../consola-de-registro/ConsolaDeRegistro.tsx";
 
 function ConsolaDeAutorización(): ReactElement {
     return (
         <div className={"consola-de-autorización"}>
-
+            <ConsolaDeRegistro/>
+            <ConsolaDeInicioDeSesión/>
         </div>
     );
 }
