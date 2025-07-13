ed20597cc43670176d222fe5b08dc1b2144f1853
theoliverlear <sigwartholiver2@gmail.com>
2025-07-13 14:31:32 -0500

    Agregar TipoDeEntradaDeAutorización.ts para definir los tipos de entrada utilizados en las entradas de autorización

---
+++ React/src/componentes/elementos/grupos-elementos-autorización/grupos-elementos-entrada/entrada-de-autorización/modelos/TipoDeEntradaDeAutorización.ts
@@ -0,0 +1,6 @@
+export enum TipoDeEntradaDeAutorización {
+    NOMBRE_DE_USUARIO = "Nombre de Usuario",
+    CONTRASEÑA = "Contraseña",
+    CONFIRMAR_CONTRASEÑA = "Confirmar Contraseña",
+    CORREO_ELECTRÓNICO = "Correo Electrónico",
+}
