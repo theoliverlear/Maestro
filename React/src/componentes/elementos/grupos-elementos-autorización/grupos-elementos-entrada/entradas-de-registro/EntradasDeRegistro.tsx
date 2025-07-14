8133cfacc74111e4825ce2c7029ee557a5acb314
theoliverlear <sigwartholiver2@gmail.com>
2025-07-14 13:09:46 -0500

    Edite EntradasDeRegistro.tsx para agregar manejo de estado, efectos secundarios y controladores de entrada para capturar y validar datos de registro

--- React/src/componentes/elementos/grupos-elementos-autorización/grupos-elementos-entrada/entradas-de-registro/EntradasDeRegistro.tsx
+++ React/src/componentes/elementos/grupos-elementos-autorización/grupos-elementos-entrada/entradas-de-registro/EntradasDeRegistro.tsx
@@ -1,5 +1,5 @@
 import './EntradasDeRegistro.scss';
-import {ReactElement} from "react";
+import {ReactElement, useEffect, useState} from "react";
 import EntradaDeAutorización
     from "../entrada-de-autorización/EntradaDeAutorización.tsx";
 import {
@@ -8,16 +8,45 @@ import {
 import Título from "../../../grupos-elementos-texto/título/Título.tsx";
 import {TipoDeEtiqueta} from "../../../../../modelos/html/TipoDeEtiqueta.ts";
 import Botón from "../../../grupos-elementos-nativos/botón/Botón.tsx";
+import {
+    GenSolicitudDeRegistro
+} from "../../../../../modelos/autorización/GenSolicitudDeRegistro.ts";
+import {
+    EstadosDeValidezDeAutorización
+} from "../../modelos/EstadosDeValidezDeAutorización.ts";
+import {
+    usarEntradasDeRegistro
+} from "../../../../../manos/usarEntradasDeRegistro.ts";
 
-function EntradasDeRegistro(): ReactElement {
+interface PropsEntradasDeRegistro {
+    cambioPetición: (solicitud: GenSolicitudDeRegistro) => void;
+    sobreCambioDeMensaje: (mensaje: EstadosDeValidezDeAutorización) => void;
+    alEnviar: (éxito: boolean) => void;
+}
+
+function EntradasDeRegistro(props: PropsEntradasDeRegistro): ReactElement {
+    const { solicitud, mensaje, manipuladores } = usarEntradasDeRegistro();
+
+    useEffect(() => {
+        props.cambioPetición(solicitud);
+    }, [props.cambioPetición, solicitud]);
+
+    useEffect(() => {
+        props.sobreCambioDeMensaje(mensaje);
+    }, [props.sobreCambioDeMensaje, mensaje]);
+
     return (
         <div className={"entradas-de-registro"}>
             <Título texto={"Registro"} tipoDeEtiqueta={TipoDeEtiqueta.H3}/>
-            <EntradaDeAutorización tipo={TipoDeEntradaDeAutorización.NOMBRE_DE_USUARIO}/>
-            <EntradaDeAutorización tipo={TipoDeEntradaDeAutorización.CORREO_ELECTRÓNICO}/>
-            <EntradaDeAutorización tipo={TipoDeEntradaDeAutorización.CONTRASEÑA}/>
-            <EntradaDeAutorización tipo={TipoDeEntradaDeAutorización.CONFIRMAR_CONTRASEÑA}/>
-            <Botón texto={"Registro"}/>
+            <EntradaDeAutorización tipo={TipoDeEntradaDeAutorización.NOMBRE_DE_USUARIO}
+                                   enLaEntrada={manipuladores.manejarNombreDeUsuario}/>
+            <EntradaDeAutorización tipo={TipoDeEntradaDeAutorización.CORREO_ELECTRÓNICO}
+                                   enLaEntrada={manipuladores.manejarCorreoElectrónico}/>
+            <EntradaDeAutorización tipo={TipoDeEntradaDeAutorización.CONTRASEÑA}
+                                   enLaEntrada={manipuladores.manejarContraseña}/>
+            <EntradaDeAutorización tipo={TipoDeEntradaDeAutorización.CONFIRMAR_CONTRASEÑA}
+                                   enLaEntrada={manipuladores.manejarConfirmarContraseña}/>
+            <Botón texto={"Registro"} alHacerClic={() => props.alEnviar(manipuladores.manejarEnvío())}/>
         </div>
     );
 }
