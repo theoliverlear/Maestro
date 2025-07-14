ebe9d757bf722a4de852e3e34a8a884a65ff908f
theoliverlear <sigwartholiver2@gmail.com>
2025-07-14 13:08:16 -0500

    Edite ConsolaDeRegistro.tsx para agregar manejo de estado, gestión de solicitudes de registro y actualización del mensaje de autorización

--- React/src/componentes/elementos/grupos-elementos-autorización/consola-de-registro/ConsolaDeRegistro.tsx
+++ React/src/componentes/elementos/grupos-elementos-autorización/consola-de-registro/ConsolaDeRegistro.tsx
@@ -1,12 +1,45 @@
 import './ConsolaDeRegistro.scss';
-import {ReactElement} from "react";
+import {ReactElement, useState} from "react";
 import EntradasDeRegistro
     from "../grupos-elementos-entrada/entradas-de-registro/EntradasDeRegistro.tsx";
+import MensajeDeAutorización
+    from "../mensaje-de-autorización/MensajeDeAutorización.tsx";
+import {
+    EstadosDeValidezDeAutorización
+} from "../modelos/EstadosDeValidezDeAutorización.ts";
+import {
+    GenSolicitudDeRegistro
+} from "../../../../modelos/autorización/GenSolicitudDeRegistro.ts";
+import {
+    ServicioDeRegistroHttp
+} from "../../../../servicios/http/autorización/ServicioDeRegistroHttp.ts";
+import {usarInyectar} from "../../../../servicios/id/ProveedorDeServicios.ts";
 
 function ConsolaDeRegistro(): ReactElement {
+    const [mensajeDeAutorización, asignarMensajeDeAutorización] = useState<EstadosDeValidezDeAutorización>(EstadosDeValidezDeAutorización.VÁLIDO);
+    const [solicitudDeRegistro, asignarSolicitudDeRegistro] = useState<GenSolicitudDeRegistro>(new GenSolicitudDeRegistro());
+
+    const servicioDeRegistroHttp: ServicioDeRegistroHttp = usarInyectar(ServicioDeRegistroHttp);
+    function gestionarCambioDeSolicitud(solicitud: GenSolicitudDeRegistro): void {
+        asignarSolicitudDeRegistro(solicitud);
+    }
+
+    function manejarCambioDeMensaje(mensaje: EstadosDeValidezDeAutorización): void {
+        asignarMensajeDeAutorización(mensaje);
+    }
+
+    async function manejarEnvío(éxito: boolean): Promise<void> {
+        if (éxito) {
+            await servicioDeRegistroHttp.registrar(solicitudDeRegistro.obtenerModelo());
+        }
+    }
+
     return (
         <div className={"consola-de-registro"}>
-            <EntradasDeRegistro/>
+            <EntradasDeRegistro cambioPetición={gestionarCambioDeSolicitud}
+                                sobreCambioDeMensaje={manejarCambioDeMensaje}
+                                alEnviar={manejarEnvío}/>
+            <MensajeDeAutorización mensaje={mensajeDeAutorización}/>
         </div>
     );
 }
