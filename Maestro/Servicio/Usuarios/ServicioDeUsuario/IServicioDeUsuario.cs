54fb2ee22378207a0938bb67bf4ef87f6e1e0655
theoliverlear <sigwartholiver2@gmail.com>
2025-07-19 16:55:31 -0500

    Agregar importación de espacio de nombres para `ServicioDeBaseDatos` en IServicioDeUsuario.cs

--- Maestro/Servicio/Usuarios/ServicioDeUsuario/IServicioDeUsuario.cs
+++ Maestro/Servicio/Usuarios/ServicioDeUsuario/IServicioDeUsuario.cs
@@ -1,5 +1,6 @@
 using Maestro.Comunicación.Solicitud.Autorización;
 using Maestro.Entidad.Usuario;
+using Maestro.Servicio.BaseDatos.ServicioDeBaseDatos;
 
 namespace Maestro.Servicio.Usuarios.ServicioDeUsuario;
 
