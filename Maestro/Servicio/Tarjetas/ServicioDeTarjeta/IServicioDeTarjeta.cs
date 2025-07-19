3f9130142b4e9bc67ccdd768b6d5874ca3c78d38
theoliverlear <sigwartholiver2@gmail.com>
2025-07-19 17:15:00 -0500

    Agregue IServicioDeTarjeta.cs para definir una interfaz de servicio para gestionar `Tarjeta` en el back-end

---
+++ Maestro/Servicio/Tarjetas/ServicioDeTarjeta/IServicioDeTarjeta.cs
@@ -0,0 +1,9 @@
+using Maestro.Entidad.Tarjeta;
+using Maestro.Servicio.BaseDatos.ServicioDeBaseDatos;
+
+namespace Maestro.Servicio.Tarjetas.ServicioDeTarjeta;
+
+public interface IServicioDeTarjeta : IServicioDeBaseDatos<Tarjeta>
+{
+
+}
