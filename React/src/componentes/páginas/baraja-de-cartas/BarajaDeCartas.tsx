4848af61e237465a1fbb39bee62ae475d6b49123
theoliverlear <sigwartholiver2@gmail.com>
2025-08-07 20:23:19 -0500

    Agregue lógica en BarajaDeCartas.tsx para mostrar u ocultar el menú de creación de barajas con estado y nuevos componentes

--- React/src/componentes/páginas/baraja-de-cartas/BarajaDeCartas.tsx
+++ React/src/componentes/páginas/baraja-de-cartas/BarajaDeCartas.tsx
@@ -1,10 +1,19 @@
 import './BarajaDeCartas.scss';
-import {ReactElement} from "react";
+import {ReactElement, useState} from "react";
+import BotónParaCrearBaraja
+    from "../../elementos/grupos-elementos-baraja-de-cartas/botón-para-crear-baraja/BotónParaCrearBaraja.tsx";
+import MenúParaCrearBaraja
+    from "../../elementos/grupos-elementos-baraja-de-cartas/menú-para-crear-baraja/MenúParaCrearBaraja.tsx";
 
 function BarajaDeCartas(): ReactElement {
+    const [barajaCrearMenúVisible, asignarBarajaCrearMenúVisible] = useState<boolean>(false);
+    function manejarClicEnMenú(visible: boolean): void {
+        asignarBarajaCrearMenúVisible(visible);
+    }
     return (
         <div className={"baraja-de-cartas"}>
-
+            {!barajaCrearMenúVisible && <BotónParaCrearBaraja alHacerClic={manejarClicEnMenú}/>}
+            {barajaCrearMenúVisible && <MenúParaCrearBaraja/>}
         </div>
     );
 }
