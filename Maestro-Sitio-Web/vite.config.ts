84ca50ea5417b3a5d894e1b288e468a5902a878a
Oliver Lear Sigwarth <sigwartholiver2@gmail.com>
2026-06-01 15:44:26 -0500

    Agregar TailwindCSS como plugin en vite.config.ts para mejorar manejo de estilos en Maestro-Sitio-Web

--- Maestro-Sitio-Web/vite.config.ts
+++ Maestro-Sitio-Web/vite.config.ts
@@ -1,11 +1,13 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
 
     plugins: [
-        react()
+        react(),
+        tailwindcss()
     ],
     cacheDir: './node_modules/.vite',
     css: {
