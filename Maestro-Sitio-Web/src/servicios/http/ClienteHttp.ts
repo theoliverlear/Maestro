00846882fe9c8d3f4b43b156e971eaee48178830
Oliver Lear Sigwarth <sigwartholiver2@gmail.com>
2026-06-01 15:54:12 -0500

    Refactorizar ClienteHttp.ts para eliminar puntos y comas y usar declaraciones `const`

--- Maestro-Sitio-Web/src/servicios/http/ClienteHttp.ts
+++ Maestro-Sitio-Web/src/servicios/http/ClienteHttp.ts
@@ -1,63 +1,59 @@
-import {injectable} from "tsyringe";
-import {http} from "./http.ts";
+import {injectable} from "tsyringe"
+import {http} from "./http.ts"
 
 @injectable()
 export class ClienteHttp<Enviar, Recibir> {
-    private _url: string;
+    private _url: string
 
     constructor(url: string) {
-        this._url = url;
+        this._url = url
     }
 
     get url(): string {
-        return this._url;
+        return this._url
     }
 
     set url(nuevaUrl: string) {
-        this._url = nuevaUrl;
+        this._url = nuevaUrl
     }
 
     public async get(): Promise<Recibir> {
-        let respuesta: Recibir | null;
-        respuesta = await http.get(this._url).then(cargaÚtil => {
-            return cargaÚtil.data as Recibir;
-        });
+        const respuesta: Recibir | null = await http.get(this._url).then(cargaÚtil => {
+            return cargaÚtil.data as Recibir
+        })
         if (!respuesta) {
             throw new Error("Error al llamar a una solicitud GET.")
         }
-        return respuesta;
+        return respuesta
     }
 
     public async post(cargaÚtil: Enviar): Promise<Recibir> {
-        let respuesta: Recibir | null;
-        respuesta = await http.post(this._url, cargaÚtil).then(cargaÚtil => {
-            return cargaÚtil.data as Recibir;
-        });
+        const respuesta: Recibir = await http.post(this._url, cargaÚtil).then(cargaÚtil => {
+            return cargaÚtil.data as Recibir
+        })
         if (!respuesta) {
             throw new Error("Error al llamar a una solicitud POST.")
         }
-        return respuesta;
+        return respuesta
     }
 
     public async put(cargaÚtil: Enviar): Promise<Recibir> {
-        let respuesta: Recibir | null;
-        respuesta = await http.put(this._url, cargaÚtil).then(cargaÚtil => {
-            return cargaÚtil.data as Recibir;
-        });
+        const respuesta: Recibir = await http.put(this._url, cargaÚtil).then(cargaÚtil => {
+            return cargaÚtil.data as Recibir
+        })
         if (!respuesta) {
             throw new Error("Error al llamar a una solicitud PUT.")
         }
-        return respuesta;
+        return respuesta
     }
 
     public async delete(): Promise<Recibir> {
-        let respuesta: Recibir | null;
-        respuesta = await http.delete(this._url).then(cargaÚtil => {
-            return cargaÚtil.data as Recibir;
-        });
+        const respuesta: Recibir = await http.delete(this._url).then(cargaÚtil => {
+            return cargaÚtil.data as Recibir
+        })
         if (!respuesta) {
             throw new Error("Error al llamar a una solicitud DELETE.")
         }
-        return respuesta;
+        return respuesta
     }
 }
