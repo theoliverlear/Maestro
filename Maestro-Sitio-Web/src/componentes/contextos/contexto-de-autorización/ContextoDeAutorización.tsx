9436cff99b6f0d3692ba27d703887a67d7e77a5f
Oliver Lear Sigwarth <sigwartholiver2@gmail.com>
2026-06-01 19:22:00 -0500

    Refactorizar ContextoDeAutorización.tsx para eliminar puntos y comas innecesarios en línea con las reglas de estilo de código

--- Maestro-Sitio-Web/src/componentes/contextos/contexto-de-autorización/ContextoDeAutorización.tsx
+++ Maestro-Sitio-Web/src/componentes/contextos/contexto-de-autorización/ContextoDeAutorización.tsx
@@ -6,46 +6,46 @@ import {
     useContext,
     useEffect,
     useMemo
-} from "react";
-import {PosibleIContextoDeAutorización} from "./modelos/tipos.ts";
-import {usarConectado} from "../../../manos/usarConectado.ts";
+} from "react"
+import {PosibleIContextoDeAutorización} from "./modelos/tipos.ts"
+import {usarConectado} from "../../../manos/usarConectado.ts"
 
 export interface IContextoDeAutorización {
     conectado: boolean;
     refrescar: () => Promise<void>;
 }
 
-const ContextoDeAutorización = createContext<PosibleIContextoDeAutorización>(null);
+const ContextoDeAutorización = createContext<PosibleIContextoDeAutorización>(null)
 
 export function ProveedorDeAutorización({ children }: { children: ReactNode }) {
     const {
         conectado,
         estáAutorizado
-    } = usarConectado();
+    } = usarConectado()
 
     useEffect(() => {
         (async () => {
-           await estáAutorizado();
-        })();
-    }, []);
+           await estáAutorizado()
+        })()
+    }, [])
 
     const valor = useMemo(() => ({ conectado, refrescar: estáAutorizado }),
-        [conectado, estáAutorizado]);
+        [conectado, estáAutorizado])
     
     return (
         <ContextoDeAutorización.Provider value={valor}>
             {children}
         </ContextoDeAutorización.Provider>
-    );
+    )
 }
 
 export function usarAutorización() {
-    const contexto: PosibleIContextoDeAutorización = useContext(ContextoDeAutorización);
+    const contexto: PosibleIContextoDeAutorización = useContext(ContextoDeAutorización)
     if (!contexto) {
         throw new Error("El contexto de autorización no está disponible. " +
                         "Asegúrate de envolver tu componente con " +
-                        "ProveedorDeAutorización.");
+                        "ProveedorDeAutorización.")
     }
-    return contexto;
+    return contexto
 }
 
