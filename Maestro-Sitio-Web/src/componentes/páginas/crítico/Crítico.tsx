46e9a21ff967f59adfe177d16204f6408a96f736
Oliver Lear Sigwarth <sigwartholiver2@gmail.com>
2026-06-01 20:11:58 -0500

    Agregar comentario TODO en Crítico.tsx para incluir perfiles de personalidad en la crítica contextual

--- Maestro-Sitio-Web/src/componentes/páginas/crítico/Crítico.tsx
+++ Maestro-Sitio-Web/src/componentes/páginas/crítico/Crítico.tsx
@@ -1,2 +1,5 @@
 ﻿// TODO: Agregar un componente para criticar y mejorar de oración de entrada.
 //       Usar OpenAI API.
+
+// TODO: Agregar perfiles de personalidad para la crítica contextual, tales
+//       como formal, conversacional, jerga y otros contextos específicos.
