168501f3827fc2ffa5a0e55a3910e59ca7cd4d11
Oliver Lear Sigwarth <sigwartholiver2@gmail.com>
2026-06-01 20:35:28 -0500

    Expandir IUsuariosDeRepositorio.cs para agregar métodos `ObtenerPorId()` y `ObtenerPorCorreoElectrónico()`

--- Maestro-Biblioteca/Biblioteca-Api/Repositorios-Api/Repositorio/Usuarios/IUsuariosDeRepositorio.cs
+++ Maestro-Biblioteca/Biblioteca-Api/Repositorios-Api/Repositorio/Usuarios/IUsuariosDeRepositorio.cs
@@ -4,5 +4,7 @@ namespace Maestro.Repositorio.Usuarios;
 
 public interface IUsuariosDeRepositorio : IRepositorio
 {
+    public Usuario? ObtenerPorId(int id);
     public Usuario? ObtenerPorNombreDeUsuario(string nombreDeUsuario);
+    public Usuario? ObtenerPorCorreoElectrónico(string correoElectrónico);
 }
