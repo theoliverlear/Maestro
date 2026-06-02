d93c147b7ab8f219aafd3d12461c88b95b7b815e
Oliver Lear Sigwarth <sigwartholiver2@gmail.com>
2026-06-02 18:50:42 -0500

    Agregar SolicitudCrítico.cs para representar solicitudes con tipo y carga en formato JSON en Biblioteca-Crítico

---
+++ Maestro-Biblioteca/Biblioteca-Crítico/Comunicacion-Crítico/Comunicacion/Solicitud/SolicitudCrítico.cs
@@ -0,0 +1,7 @@
+namespace Maestro.Biblioteca.Crítico.Comunicacion.Solicitud;
+
+public class SolicitudCrítico
+{
+    public string Tipo { get; set; } = string.Empty;
+    public string CargaJson { get; set; } = "{}";
+}
