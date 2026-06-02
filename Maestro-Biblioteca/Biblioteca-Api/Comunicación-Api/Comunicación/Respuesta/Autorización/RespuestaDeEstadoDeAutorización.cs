295ba8e2c8ced70d433278a37b94c138f291cf48
Oliver Lear Sigwarth <sigwartholiver2@gmail.com>
2026-06-01 20:32:58 -0500

    Expandir RespuestaDeEstadoDeAutorización.cs para incluir propiedades de token, token de actualización, y fecha de expiración, con atributos `JsonIgnore` y constructor actualizado

--- Maestro-Biblioteca/Biblioteca-Api/Comunicación-Api/Comunicación/Respuesta/Autorización/RespuestaDeEstadoDeAutorización.cs
+++ Maestro-Biblioteca/Biblioteca-Api/Comunicación-Api/Comunicación/Respuesta/Autorización/RespuestaDeEstadoDeAutorización.cs
@@ -1,11 +1,24 @@
+using System.Text.Json.Serialization;
+
 namespace Maestro.Comunicación.Respuesta.Autorización;
 
 public class RespuestaDeEstadoDeAutorización
 {
     public bool EsAutorizado { get; private set; }
+    public string? Token { get; private set; }
+    [JsonIgnore]
+    public string? IdDeTokenDeActualización { get; private set; }
+    [JsonIgnore]
+    public DateTimeOffset? ExpiraTokenDeActualizaciónEn { get; private set; }
 
-    public RespuestaDeEstadoDeAutorización(bool esAutorizado)
+    public RespuestaDeEstadoDeAutorización(bool esAutorizado,
+                                           string? token = null,
+                                           string? idDeTokenDeActualización = null,
+                                           DateTimeOffset? expiraTokenDeActualizaciónEn = null)
     {
         this.EsAutorizado = esAutorizado;
+        this.Token = token;
+        this.IdDeTokenDeActualización = idDeTokenDeActualización;
+        this.ExpiraTokenDeActualizaciónEn = expiraTokenDeActualizaciónEn;
     }
 }
