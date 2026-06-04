46f498a0f979e54eb7ce35add046fea9fc2919bc
Oliver Lear Sigwarth <sigwartholiver2@gmail.com>
2026-06-03 19:30:29 -0500

    Agregar OpcionesDeCaracteristicasUniversales.cs para definir opciones predeterminadas de carga y cosecha de datos en Componentes-Universal

---
+++ Maestro-Biblioteca/Biblioteca-Universal/Componentes-Universal/Componentes/OpcionesDeCaracteristicasUniversales.cs
@@ -0,0 +1,9 @@
+namespace Maestro.Biblioteca.Universal.Componentes;
+
+public class OpcionesDeCaracteristicasUniversales
+{
+    public const string Seccion = "Caracteristicas";
+
+    public bool CargarDatosExternos { get; set; } = true;
+    public bool CosecharDatosExternos { get; set; } = true;
+}
