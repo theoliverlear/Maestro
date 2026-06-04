ba4ee24f9939aff96ea0509c5cd22c2a3a81d8c0
Oliver Lear Sigwarth <sigwartholiver2@gmail.com>
2026-06-03 19:02:27 -0500

    Agregar RegistroDeEventosKafka.cs para registrar eventos universales de Kafka en Configuración de Eventos-Universal

---
+++ Maestro-Biblioteca/Biblioteca-Universal/Eventos-Universal/Eventos/Configuracion/RegistroDeEventosKafka.cs
@@ -0,0 +1,17 @@
+using Maestro.Biblioteca.Universal.Eventos.Kafka;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Maestro.Biblioteca.Universal.Eventos.Configuracion;
+
+public static class RegistroDeEventosKafka
+{
+    public static IServiceCollection RegistrarEventosKafkaUniversales(this IServiceCollection servicios,
+                                                                      IConfiguration configuracion)
+    {
+        servicios.Configure<OpcionesDeKafka>(configuracion.GetSection(OpcionesDeKafka.Seccion));
+        servicios.AddSingleton<ProveedorDeTemasKafka>();
+        servicios.AddSingleton<IPublicadorDeEventos, PublicadorKafkaDeEventos>();
+        return servicios;
+    }
+}
