e8d41a106638c58e143defea9b7f518a902aaf7e
Oliver Lear Sigwarth <sigwartholiver2@gmail.com>
2026-06-03 19:08:38 -0500

    Agregar Ansi.cs para definir constantes de códigos de escape ANSI en Modelos-Universal

---
+++ Maestro-Biblioteca/Biblioteca-Universal/Modelos-Universal/Modelos/Ansi.cs
@@ -0,0 +1,16 @@
+namespace Maestro.Biblioteca.Universal.Modelos;
+
+public static class Ansi
+{
+    public const string Reiniciar = "\u001B[0m";
+    public const string Subrayado = "\u001B[4m";
+    public const string Negrita = "\u001B[1m";
+    public const string Cian = "\u001B[36m";
+    public const string Magenta = "\u001B[35m";
+    public const string Azul = "\u001B[34m";
+    public const string Verde = "\u001B[32m";
+    public const string Amarillo = "\u001B[33m";
+    public const string Blanco = "\u001B[37m";
+    public const string Rojo = "\u001B[31m";
+    public const string Gris = "\u001B[90m";
+}
