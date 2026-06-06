8a387cf13c918d30667b6aac1c479a945938378f
Oliver Lear Sigwarth <sigwartholiver2@gmail.com>
2026-06-05 19:02:27 -0500

    Eliminar clases y configuraciones no utilizadas de Biblioteca-Crítico y Biblioteca-Universal para simplificación del código

--- Maestro-Biblioteca/Biblioteca-Crítico/Comunicacion-Crítico/Comunicacion/Solicitud/SolicitudCrítico.cs
+++
@@ -1,7 +0,0 @@
-namespace Maestro.Biblioteca.Crítico.Comunicacion.Solicitud;
-
-public class SolicitudCrítico
-{
-    public string Tipo { get; set; } = string.Empty;
-    public string CargaJson { get; set; } = "{}";
-}
--- Maestro-Biblioteca/Biblioteca-Universal/Componentes-Universal/Componentes/OpcionesDeCaracteristicasUniversales.cs
+++
@@ -1,9 +0,0 @@
-namespace Maestro.Biblioteca.Universal.Componentes;
-
-public class OpcionesDeCaracteristicasUniversales
-{
-    public const string Seccion = "Caracteristicas";
-
-    public bool CargarDatosExternos { get; set; } = true;
-    public bool CosecharDatosExternos { get; set; } = true;
-}
