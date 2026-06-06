d2a4d917e9e993b43a8e8b5301c1c05d4f553d7e
Oliver Lear Sigwarth <sigwartholiver2@gmail.com>
2026-06-06 14:54:40 -0500

    Actualizar Programa.cs para cargar secretos de AWS, configurar logging y registrar servicios de traducción para Maestro-Traducción

--- Maestro-Traducción/Programa.cs
+++ Maestro-Traducción/Programa.cs
@@ -1,4 +1,8 @@
+using Maestro.Biblioteca.Traducción.Eventos;
+using Maestro.Biblioteca.Traducción.Servicios;
+using Maestro.Biblioteca.Universal.Configuración;
 using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
 
 HostApplicationBuilderSettings configuración = new()
 {
@@ -6,5 +10,13 @@ HostApplicationBuilderSettings configuración = new()
     ContentRootPath = AppContext.BaseDirectory
 };
 HostApplicationBuilder constructor = Host.CreateApplicationBuilder(configuración);
+await constructor.Configuration.CargarSecretosAws(constructor.Environment);
+constructor.Logging.ClearProviders();
+constructor.Logging.AddConsole();
+
+constructor.Services.RegistrarBibliotecaUniversal(constructor.Configuration);
+constructor.Services.RegistrarServiciosTraducción();
+constructor.Services.RegistrarConsumidorDeSolicitudesTraducción();
+
 IHost app = constructor.Build();
 await app.RunAsync();
