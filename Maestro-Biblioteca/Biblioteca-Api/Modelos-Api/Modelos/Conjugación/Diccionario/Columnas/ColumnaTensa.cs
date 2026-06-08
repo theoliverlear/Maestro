8071483b1b24b8f686c6428d1d30d98185a8ecd3
Oliver Lear Sigwarth <sigwartholiver2@gmail.com>
2026-06-08 17:57:38 -0500

    Renombrar Tenso.cs a ColumnaTensa.cs y actualizar referencias para reflejar el cambio de nombre en Modelos-Api

--- Maestro-Biblioteca/Biblioteca-Api/Modelos-Api/Modelos/Conjugación/Diccionario/Columnas/ColumnaTensa.cs
+++ Maestro-Biblioteca/Biblioteca-Api/Modelos-Api/Modelos/Conjugación/Diccionario/Columnas/ColumnaTensa.cs
@@ -1,32 +1,32 @@
 namespace Maestro.Biblioteca.Api.Modelos.Conjugación.Diccionario.Columnas;
 
-public class Tenso
+public class ColumnaTensa
 {
     public string CadenaDeTenso { get; set; }
-    public static readonly Tenso Presente = new("Presente");
-    public static readonly Tenso Futuro = new("Futuro");
-    public static readonly Tenso Imperfecto = new("Imperfecto");
-    public static readonly Tenso Pretérito = new("Pretérito");
-    public static readonly Tenso Condicional = new("Condicional");
-    public static readonly Tenso PresentePerfecto = new("Presente perfecto");
-    public static readonly Tenso FuturoPerfecto = new("Futuro perfecto");
-    public static readonly Tenso Pluscuamperfecto = new("Pluscuamperfecto");
-    public static readonly Tenso PretéritoAnterior = new("Pretérito anterior");
-    public static readonly Tenso CondicionalPerfecto = new("Condicional perfecto");
+    public static readonly ColumnaTensa Presente = new("Presente");
+    public static readonly ColumnaTensa Futuro = new("Futuro");
+    public static readonly ColumnaTensa Imperfecto = new("Imperfecto");
+    public static readonly ColumnaTensa Pretérito = new("Pretérito");
+    public static readonly ColumnaTensa Condicional = new("Condicional");
+    public static readonly ColumnaTensa PresentePerfecto = new("Presente perfecto");
+    public static readonly ColumnaTensa FuturoPerfecto = new("Futuro perfecto");
+    public static readonly ColumnaTensa Pluscuamperfecto = new("Pluscuamperfecto");
+    public static readonly ColumnaTensa PretéritoAnterior = new("Pretérito anterior");
+    public static readonly ColumnaTensa CondicionalPerfecto = new("Condicional perfecto");
 
-    public static readonly List<Tenso> TiemposVerbales = new()
+    public static readonly List<ColumnaTensa> TiemposVerbales = new()
     {
         Presente, Futuro, Imperfecto, Pretérito, Condicional,
         PresentePerfecto, FuturoPerfecto, Pluscuamperfecto, PretéritoAnterior,
         CondicionalPerfecto
     };
 
-    private Tenso(string tenso)
+    private ColumnaTensa(string tenso)
     {
         this.CadenaDeTenso = tenso;
     }
 
-    public static Tenso DeCuerda(string tenso)
+    public static ColumnaTensa DeCuerda(string tenso)
     {
         tenso = tenso[0].ToString().ToUpper() + tenso[1..].ToLower();
         return tenso switch
