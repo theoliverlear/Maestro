9e3b6db05faa3fbb04ce01077cc02c2e31649242
Oliver Lear Sigwarth <sigwartholiver2@gmail.com>
2026-06-08 18:05:52 -0500

    Agregar Tenso.cs para representar propiedades de conjugación relacionadas con tiempos verbales en Modelos-Api

---
+++ Maestro-Biblioteca/Biblioteca-Api/Modelos-Api/Entidad/Conjugación/Propiedades/Tenso.cs
@@ -0,0 +1,28 @@
+namespace Maestro.Biblioteca.Api.Entidad.Conjugación.Propiedades;
+
+public class Tenso
+{
+    public string CadenaDeTenso { get; }
+
+    public static readonly Tenso Presente = new("Presente");
+    public static readonly Tenso Futuro = new("Futuro");
+    public static readonly Tenso Imperfecto = new("Imperfecto");
+    public static readonly Tenso Pretérito = new("Pretérito");
+    public static readonly Tenso Condicional = new("Condicional");
+    public static readonly Tenso PresentePerfecto = new("Presente perfecto");
+    public static readonly Tenso FuturoPerfecto = new("Futuro perfecto");
+    public static readonly Tenso Pluscuamperfecto = new("Pluscuamperfecto");
+    public static readonly Tenso PretéritoAnterior = new("Pretérito anterior");
+    public static readonly Tenso CondicionalPerfecto = new("Condicional perfecto");
+
+        public static readonly List<Tenso> Tensos = new()
+        {
+            Presente, Futuro, Imperfecto, Pretérito, Condicional, PresentePerfecto,
+            FuturoPerfecto, Pluscuamperfecto, PretéritoAnterior, CondicionalPerfecto
+        };
+    
+    public Tenso(string cadenaDeTenso)
+    {
+        CadenaDeTenso = cadenaDeTenso;
+    }
+}
